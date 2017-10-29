d478b55b9afafd20c043c8039f39934321feaea0
Dunkledore <b7dunkerley@me.com>
2017-10-29 03:10:40 +0000

    w8

---
+++ cogs/sukebe.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Rate:
+    """Rate a user."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def rate(self, ctx):
+        """Rates users. 157% accurate!"""
+        mentionlist = ctx.message.mentions
+        user = mentionlist[0]
+        random.seed(int(user.id) % int(ctx.message.timestamp.timestamp()),)
+        x = ":sparkles:" * random.randint(1, 10)
+        ctx.send("{} gets a solid: ".format(user.name) + x)
+
+
+def setup(bot):
+    bot.add_cog(Rate(bot))
