d4bfc11a810832c005fa5bbe2f2fd81dc18c7724
Seneth <eadesjames@me.com>
2017-10-28 23:03:14 -0400

    Sparkles

---
+++ cogs/sukebe.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Rate:
+    """Rate a user."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def rate(self, ctx, user: discord.Member):
+        """Rates users. 157% accurate!"""
+
+        random.seed(int(user.id) % int(ctx.message.timestamp.timestamp()),)
+        x = ":sparkles:" * random.randint(1, 10)
+        ctx.send("{} gets a solid: ".format(user.name) + x)
+
+
+def setup(bot):
+    bot.add_cog(Rate(bot))
