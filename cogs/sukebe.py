08af456224cb078e76e12f8b5a9cc75b98329f62
Dunkledore <b7dunkerley@me.com>
2017-10-29 03:28:41 +0000

    await

--- cogs/sukebe.py
+++ cogs/sukebe.py
@@ -12,7 +12,7 @@ class Rate:
     @commands.command(pass_context=True)
     async def rate(self, ctx):
         """Rates users. 157% accurate!"""
-        ctx.send(ctx.message.mentions[0].id)
+        await ctx.send(ctx.message.mentions[0].id)
         random.seed(int(ctx.message.mentions[0].id) % int(ctx.message.timestamp.timestamp()),)
         x = ":sparkles:" * random.randint(1, 10)
         await ctx.send("{} gets a solid: ".format(user.name) + x)
