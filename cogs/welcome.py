7a81a9201a514ebe63075e31727ce80428825c67
Dunkledore <b7dunkerley@me.com>
2017-10-29 01:27:39 +0000

    gotem

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -19,21 +19,14 @@ class Welcome:
 	@commands.group(invoke_without_command=True)
 	@commands.guild_only()
 	async def welcome(self, ctx, *, member: discord.Member = None):
-		"""Tells you command usage stats for the server or a member."""
-		await ctx.send('Will ask for message')
+		"""Tells you command usage stats for the server or a member."""s
 		await self.show_welcome_message(ctx)
 
 	async def show_welcome_message(self, ctx):
-		embed = discord.Embed(title='Welcome Message', colour=discord.Colour.blurple())
 		query = "SELECT * FROM welcome WHERE guild_id = $1;"
-		await ctx.send('about to query')
-		embed.add_field(name='Testing', value='testin')
-		await ctx.send('line')
-		await ctx.send(embed=embed)
 
 		try:
 			welcome = await ctx.db.fetchrow(query, ctx.guild.id)
-			await ctx.send('done')
 			await ctx.send(welcome)
 		except Exception as e:
 			await ctx.send(e)
