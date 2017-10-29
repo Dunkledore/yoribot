c6aea1e4de2fa8747cca6e583c59529baa608d81
Dunkledore <b7dunkerley@me.com>
2017-10-29 03:09:31 +0000

    try this

--- cogs/sukebe.py
+++ cogs/sukebe.py
@@ -10,9 +10,11 @@ class Rate:
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def rate(self, ctx, user: discord.Member):
+    async def rate(self, ctx, *, mention):
         """Rates users. 157% accurate!"""
-
+        author = ctx.message.author.id
+        mentionlist = ctx.message.mentions
+        user = mentionlist[0]
         random.seed(int(user.id) % int(ctx.message.timestamp.timestamp()),)
         x = ":sparkles:" * random.randint(1, 10)
         ctx.send("{} gets a solid: ".format(user.name) + x)
