6f6da59222c4af3941492049fa00bac4a282cdf7
Dunkledore <b7dunkerley@me.com>
2017-10-29 01:47:01 +0100

    fucking python

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -17,13 +17,13 @@ class Welcome:
 		self.bot = bot
 
 	@commands.group(invoke_without_command=True)
-    @commands.guild_only()
-    async def welcome(self, ctx, *, member: discord.Member = None):
+	@commands.guild_only()
+	async def welcome(self, ctx, *, member: discord.Member = None):
         """Tells you command usage stats for the server or a member."""
 
-        await self.show_welcome_message(ctx)
+		await self.show_welcome_message(ctx)
 
-     async def show_welcome_message(self, ctx):
+	async def show_welcome_message(self, ctx):
 		embed = discord.Embed(title='Welcome Message', colour=discord.Colour.blurple())
 		query = "SELECT * FROM welcome WHERE guild_id = $1;"
 		welcome = await ctx.db.fetch(query, ctx.guild.id)
