72952d001574a0e70d624b074f77cfee4fa81876
Dunkledore <b7dunkerley@me.com>
2017-10-29 03:15:04 +0000

    f

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -25,8 +25,6 @@ class Welcome:
 	async def show_welcome_message(self, ctx):
 		query = "SELECT * FROM welcome WHERE guild_id = $1;"
 		welcome = await ctx.db.fetch(query, ctx.guild.id)
-		servername = ctx.message.server.name
-		await ctx.send(servername)
 		#title = 'Welcome to ' + ctx.message.server.name
 		#await ctx.send(title)
 		embed = discord.Embed(title='test', colour=discord.Colour.blurple())
