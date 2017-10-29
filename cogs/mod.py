3d1cea94820b5accede4097685e7f5eaa4846d9e
Dunkledore <b7dunkerley@me.com>
2017-10-29 22:07:27 +0000

    d

--- cogs/mod.py
+++ cogs/mod.py
@@ -280,7 +280,7 @@ class Mod:
 
     async def on_reaction_add(self, reaction, user):
         print('triggered')
-        print(ord(reaction.message.reactions[0]))
+        print(ord(reaction.emoji))
         con = self.bot.pool
         query = "SELECT * FROM mod_config WHERE guild_id = $1"
         number = await con.fetchrow(query, reaction.message.guild.id)
