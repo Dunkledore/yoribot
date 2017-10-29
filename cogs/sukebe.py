0f52cd0063f837d157fb8e7f5c1e14dec51d872a
Dunkledore <b7dunkerley@me.com>
2017-10-29 03:53:24 +0000

    name discrim

--- cogs/sukebe.py
+++ cogs/sukebe.py
@@ -16,7 +16,7 @@ class Rate:
 
         random.seed(int(ctx.message.mentions[0].id) % int(ctx.message.created_at.timestamp()),)
         x = ":sparkles:" * random.randint(1, 10)
-        await ctx.send("{} gets a solid: ".format(ctx.message.mentions[0]) + x)
+        await ctx.send("{} gets a solid: ".format(ctx.message.mentions[0].name) + x)
 
 
 def setup(bot):
