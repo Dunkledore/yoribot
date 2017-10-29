1cd1ca9af944a71531486cf74e3700dd8252f7a4
Dunkledore <b7dunkerley@me.com>
2017-10-29 03:23:36 +0000

    now this

--- cogs/sukebe.py
+++ cogs/sukebe.py
@@ -13,7 +13,6 @@ class Rate:
     async def rate(self, ctx):
         """Rates users. 157% accurate!"""
         mentionlist = ctx.message.mentions[0].id
-        await ctx.send(mentionlist)
         user = mentionlist[0]
         random.seed(int(user.id) % int(ctx.message.timestamp.timestamp()),)
         x = ":sparkles:" * random.randint(1, 10)
