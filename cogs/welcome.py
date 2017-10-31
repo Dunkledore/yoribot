20e70da338c05616c9d9a377ef94174489ba47d0
Dunkledore <b7dunkerley@me.com>
2017-10-31 22:18:52 +0000

    ?

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -28,7 +28,7 @@ class Welcome:
 		#title = 'Welcome to ' + ctx.message.server.name
 		#await ctx.send(title)
 		embed = discord.Embed(title='Welcome to ' + ctx.message.guild.name, colour=discord.Colour.blurple())
-		embed.set_author(name=ctx.message.author.name, icon_url=ctx.message.guild.avatar_url)
+		embed.set_author(name=ctx.message.author.name, icon_url=ctx.message.guild.icon)
 		embed.add_field(name='User', value=ctx.message.author.mention)
 
 
