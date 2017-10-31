15ab373a93a2af5d3a27240fb3618b7494e9c20e
Dunkledore <b7dunkerley@me.com>
2017-10-31 00:42:30 +0000

    python

--- cogs/rolemanager.py
+++ cogs/rolemanager.py
@@ -52,12 +52,12 @@ class RoleManager:
             if message.content.startswith(p):
                 role = message.content[len(p):]
                 rolename = role.replace("\"", "")
-        		roleinfo = await self._roleinfo(ctx, rolename)
-        		await ctx.message.author.add_roles(role)
-            	em = discord.Embed(color=ctx.message.author.color, description="The role has been assigned to you!")
-            	em.set_author(name="Success!", icon_url="http://bit.ly/2qi2m3a")
-            	await ctx.send(embed=em)
-            	return
+                roleinfo = await self._roleinfo(ctx, rolename)
+                await ctx.message.author.add_roles(role)
+                em = discord.Embed(color=ctx.message.author.color, description="The role has been assigned to you!")
+                em.set_author(name="Success!", icon_url="http://bit.ly/2qi2m3a")
+                await ctx.send(embed=em)
+                return
 
 
     @sar.command(pass_context=True, no_pm=True)
