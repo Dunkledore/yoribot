b32aac921ca6585c0a1d1d5a2316ba81572bcc97
Dunkledore <b7dunkerley@me.com>
2017-11-02 20:09:10 +0000

    just that

--- cogs/audio.py
+++ cogs/audio.py
@@ -111,7 +111,7 @@ class Music:
                 results = ''
                 for x in range(0,5):
                     api_key = 'AIzaSyB10j5t3LxMpuedlExxcVvj0rsezTurY9w'
-                    gurl = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id="+yt_find[x*2-x]+"&key="+api_key+"&part=contentDetails"
+                    gurl = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id="+yt_find[x*2]+"&key="+api_key+"&part=contentDetails"
                     json = simplejson.loads(urllib.request.urlopen(gurl).read())
                     await ctx.send(json['items'][0]['snippet']['title'])
                     results += json['items'][0]['snippet']['title'] + '\n'
