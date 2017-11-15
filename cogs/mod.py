95fd1285997616bd5875cbf4943b4f378300b257
Dunkledore <b7dunkerley@me.com>
2017-11-15 02:57:07 +0000

    dunno

--- cogs/mod.py
+++ cogs/mod.py
@@ -501,7 +501,7 @@ class Mod:
         ban_list = await server.bans()
         await ctx.send(ban_list)
         user = self.bot.get_user(user_id)
-        await ctx.send(user)
+        await ctx.send(user.id)
         is_banned = discord.utils.get(ban_list, user=user)
         await ctx.send(is_banned)
 
