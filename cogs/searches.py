f92a80e29b151ac29bcc5dd3e972a4f690e2664f
Dunkledore <b7dunkerley@me.com>
2017-11-15 02:24:42 +0000

    find a new one

--- cogs/searches.py
+++ cogs/searches.py
@@ -48,9 +48,9 @@ class Searches:
 
         async with aiohttp.get(self.url_cat) as response:
             img = json.loads(await response.text())["file"].replace("\\/","/")
-            if img.endswith(".mp4"):
-                await ctx.send(img)
-                return
+            while img.endswith(".mp4"):
+                img = json.loads(await response.text())["file"].replace("\\/","/")
+                
             em = discord.Embed(color=ctx.message.author.color, description=" ")
             em.set_author(name="Random cat picture", icon_url="http://bit.ly/2AH8Byg")
             em.set_image(url=img)
@@ -63,9 +63,9 @@ class Searches:
 
         async with aiohttp.get(self.url_dog) as response:
             img = json.loads(await response.text())["url"]
-            if img.endswith(".mp4"):
-                await ctx.send(img)
-                return
+            while img.endswith(".mp4"):
+                img = json.loads(await response.text())["file"].replace("\\/","/")
+                
             em = discord.Embed(color=ctx.message.author.color, description=" ")
             em.set_author(name="Random dog picture", icon_url="http://bit.ly/2jotVFo")
             em.set_image(url=img)
