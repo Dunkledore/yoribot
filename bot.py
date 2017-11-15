18d1bbc11eaceebcea93b8e3525793c6949a1ffa
Seneth <eadesjames@me.com>
2017-11-14 23:19:10 -0500

    logging?

--- bot.py
+++ bot.py
@@ -20,8 +20,8 @@ Hello! I am a premium Discord bot.
 
 log = logging.getLogger(__name__)
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger = logging.getLogger('yato')
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
