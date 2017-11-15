87a7128c3db373c5e8db97162a4c70138d7451c7
Seneth <eadesjames@me.com>
2017-11-14 23:45:16 -0500

    trying this

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ Hello! I am a premium Discord bot.
 log = logging.getLogger(__name__)
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(0)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
