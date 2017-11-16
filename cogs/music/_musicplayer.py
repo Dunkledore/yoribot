683faa33c23c6f9b24c7ad3650a77795a21e565c
Dunkledore <b7dunkerley@me.com>
2017-11-16 23:20:59 +0000

    need the method

--- cogs/music/_musicplayer.py
+++ cogs/music/_musicplayer.py
@@ -18,6 +18,19 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.title = data.get('title')
         self.url = data.get('url')
 
+    @classmethod
+    async def from_url(cls, url, *, loop=None):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, ytdl.extract_info, url)
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options),
+                   data=data)
+
 class MusicPlayer:
     def __init__(self, server_id, bot):
         """Locks onto a server for easy management of various UIs
