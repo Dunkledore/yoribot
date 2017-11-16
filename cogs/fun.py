2d02bcd6518271085d3edda1cff6cb87717e50ec
Seneth <eadesjames@me.com>
2017-11-15 22:40:52 -0500

    text

--- cogs/fun.py
+++ cogs/fun.py
@@ -10,6 +10,7 @@ class Fun:
     @commands.command(pass_context=True, no_pm=True)
     async def riot(self, ctx, *text):
         """RIOT!"""
+        text = " ".join(text)
         await ctx.send('ヽ༼ຈل͜ຈ༽ﾉ **' + text + '** ヽ༼ຈل͜ຈ༽ﾉ')
 
 
