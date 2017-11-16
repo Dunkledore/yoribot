2bda8483d97c2079e727b4913b4589cb2f12f53d
Seneth <eadesjames@me.com>
2017-11-16 01:25:50 -0500

    more

--- cogs/meta.py
+++ cogs/meta.py
@@ -52,12 +52,11 @@ class Meta:
     @commands.command(hidden=True)
     async def hello(self, ctx):
         """Displays my intro message."""
-        em = discord.Embed(color=ctx.message.author.color, description=" ")
+        em = discord.Embed(color=ctx.message.author.color, description="Yori is a premium Discord bot based on the rapptz robodanny code and heavily customized with influences also from Red-DiscordBot.")
         em.set_author(name="About Yori Bot", icon_url="http://yoribot.com/wp-content/uploads/2017/11/yoriicon.png")
         em.set_image(url='https://i.gyazo.com/c7722437eb4f75a992b1871bae091230.gif')
         em.set_footer(text= "Use the help command or visit http://yoribot.com for more information.")
         await ctx.send(embed=em)
-        await ctx.send('Hello! I\'m a robot!')
 
     @commands.command(hidden=True)
     async def charinfo(self, ctx, *, characters: str, hidden=True):
