571deca336a394d9f7d8c0cce31791155c3c6a24
Dunkledore <b7dunkerley@me.com>
2017-11-16 22:29:30 +0000

    not that

--- cogs/utils/ui_embed.py
+++ cogs/utils/ui_embed.py
@@ -72,8 +72,8 @@ class UI:
 
     async def send(self):
         """Send new GUI"""
-
-        self.sent_embed = await self.channel.send(embed=self.built_embed)
+        async with self.channel.typing():
+            self.sent_embed = await self.channel.send(embed=self.built_embed)
 
     async def usend(self):
         """Edit existing GUI if available, else send new GUI"""
