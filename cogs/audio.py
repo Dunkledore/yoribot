267f6a77b2f8bf3a7141bc68f0180c8de76c297a
Seneth <eadesjames@me.com>
2017-11-16 22:16:56 -0500

    audio help

--- cogs/audio.py
+++ cogs/audio.py
@@ -10,6 +10,15 @@ class Music:
 	def __init__(self,bot):
 		self.bot = bot
 
+    @commands.group()
+    async def music(self, ctx):
+        """Will show different commands to control bot music`"""
+        em = discord.Embed(color=ctx.message.author.color, description="Yori is a premium Discord bot based on the rapptz robodanny code and heavily customized with influences also from Red-DiscordBot.")
+        em.set_author(name="Yori Advanced Music Player", icon_url="http://yoribot.com/wp-content/uploads/2017/11/yoriicon.png")
+        em.set_image(url='https://i.gyazo.com/c7722437eb4f75a992b1871bae091230.gif')
+        em.set_footer(text= "Use the help command or visit http://yoribot.com for more information.")
+        await ctx.send(embed=em)
+
 	async def on_reaction_add(self, reaction, user):
 		"""The on_message event handler for this module
 
