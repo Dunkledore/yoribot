6c395672d77f819b6d1c7670a3f805687f749503
Dunkledore <b7dunkerley@me.com>
2017-11-17 17:24:51 +0000

    oops

--- cogs/audio.py
+++ cogs/audio.py
@@ -49,7 +49,7 @@ class Music:
 				if emoji == "⏹":
 					await _data.cache[str(server.id)].stop()
 				if emoji == "⏭":
-					is_mod = await self.is_mod(user, reaction.message.channel):
+					is_mod = await self.is_mod(user, reaction.message.channel)
 					if is_mod:
 						await _data.cache[str(server.id)].skip("1")
 				if emoji == "🔀":
