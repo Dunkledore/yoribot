cc40e393c270132eea17fd664fe8c048b8297d2d
Seneth <eadesjames@me.com>
2017-11-16 23:23:28 -0500

    ok progress

--- cogs/musichelp.py
+++ cogs/musichelp.py
@@ -1,7 +1,6 @@
-from discord.ext import commands
-from .utils import checks, formats
-from .utils.paginator import HelpPaginator, CannotPaginate
 import discord
+from discord.ext import commands
+from .utils import checks
 
 class Musichelp:
     """Use this to display the help info for the music plugin."""
@@ -11,9 +10,11 @@ class Musichelp:
 
     @commands.command()
     async def musichelp(self, ctx):
-        """Displays my intro message."""
-        em = discord.Embed(color=ctx.message.author.color, description="Yori is a premium Discord bot based on the rapptz robodanny code and heavily customized with influences also from Red-DiscordBot.")
+        em = discord.Embed(color=ctx.message.author.color, description="The following commands can be used to control the music player:")
         em.set_author(name="About Yori Bot", icon_url="http://yoribot.com/wp-content/uploads/2017/11/yoriicon.png")
+        em.add_field(name='play', value='Play a song or playlist from YouTube.', inline=False)
+        em.add_field(name='playnext', value='Will play the indicated song next.', inline=False)
+        em.add_field(name='playnow', value='Stops currently playing song to play another.', inline=False)
         em.set_image(url='https://i.gyazo.com/c7722437eb4f75a992b1871bae091230.gif')
         em.set_footer(text= "Use the help command or visit http://yoribot.com for more information.")
         await ctx.send(embed=em)
