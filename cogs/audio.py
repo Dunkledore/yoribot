6af51b5f2481fffdd31852b5bf9819ae4bd5bdbc
Dunkledore <b7dunkerley@me.com>
2017-11-17 19:47:31 +0000

    eh

--- cogs/audio.py
+++ cogs/audio.py
@@ -22,6 +22,8 @@ class Music:
 
 	def has_majority(self, reaction):
 		listeners = len(reaction.message.guild.voice_client.channel.members)
+		print (reaction.count)
+		print (listeners)
 		return (reaction.count-1) > (listeners-1/2)
 
 	async def on_reaction_add(self, reaction, user):
