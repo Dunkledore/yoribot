fea069706843a898befe9c7831950dea893fecf5
Dunkledore <b7dunkerley@me.com>
2017-11-17 19:05:48 +0000

    reactions reset after each song

--- cogs/music/_musicplayer.py
+++ cogs/music/_musicplayer.py
@@ -589,6 +589,9 @@ class MusicPlayer:
 
             self.statuslog.info("Playing")
             self.nowplayinglog.info(songname)
+
+            self.sent_embed.clear_reactions()
+            self.add_reactions()
         else:
             self.statuslog.info("Finished queue")
             self.state = "ready"
