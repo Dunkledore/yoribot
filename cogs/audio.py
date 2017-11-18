d49c975eef780875cc0795a5318372afd7c0d414
Dunkledore <b7dunkerley@me.com>
2017-11-18 13:35:12 +0000

    hmm

--- cogs/audio.py
+++ cogs/audio.py
@@ -85,7 +85,7 @@ class Music:
 		channel = message.channel
 		content = message.content
 
-		if _data.cache[str(server.id)]:
+		if _data.cache:
 			if message.channel.id == _data.cache[str(server.id)].embed.sent_embed.channel.id:
 				self.message_counter += 1
 				if self.message_counter > 5:
