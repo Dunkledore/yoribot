6bc60ec58c9039f8f26addb928f45850fbbba7e9
Seneth <eadesjames@me.com>
2017-11-20 11:38:54 -0500

    maybe

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -73,7 +73,7 @@ class Nsfw:
             image = soup.find(id="highres").get("href")
             em = discord.Embed(color=ctx.message.author.color, description=" ")
             em.set_author(name="Random Image from Konachan:", icon_url="http://bit.ly/2hHIfF6")
-            em.set_image(url='https:'+ image)
+            em.set_image(url=image)
             em.set_footer(text= "Random image from https://konachan.com")
             await ctx.send(embed=em)
         except Exception as e:
@@ -91,7 +91,7 @@ class Nsfw:
             image = soup.find(id="image").get("src")
             em = discord.Embed(color=ctx.message.author.color, description=" ")
             em.set_author(name="Random Image from rule34:", icon_url="http://bit.ly/2hHIfF6")
-            em.set_image(url='http:'+ image)
+            em.set_image(url=image)
             em.set_footer(text= "Random image from https://rule34.xxx")
             await ctx.send(embed=em)
         except Exception as e:
@@ -127,7 +127,7 @@ class Nsfw:
             image = soup.find(id="image").get("src")
             em = discord.Embed(color=ctx.message.author.color, description=" ")
             em.set_author(name="Random Image from tibib:", icon_url="http://bit.ly/2hHIfF6")
-            em.set_image(url="http:"+image)
+            em.set_image(url=image)
             em.set_footer(text= "Random image from https://www.tbib.org/")
             await ctx.send(embed=em)
         except Exception as e:
