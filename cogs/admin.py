99f0dc450b7373987eb5e56d5a169ad8098f3c76
Dunkledore <b7dunkerley@me.com>
2017-11-20 22:09:31 +0000

    oops

--- cogs/admin.py
+++ cogs/admin.py
@@ -43,7 +43,7 @@ class Admin:
         await ctx.send('Pong')
 
     @commands.command(hidden=True)
-    async def guilds(self, ctx)
+    async def guilds(self, ctx):
         """List of guilds the bot is in"""
         string = "List of  guilds the bot is in: \n"
         for guild in self.bot.guilds:
