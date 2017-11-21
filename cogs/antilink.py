245e4ee28fd91ec5d0a57823f11fe177f5a839d9
Seneth <eadesjames@me.com>
2017-11-20 21:16:59 -0500

    antilink

--- cogs/antilink.py
+++ cogs/antilink.py
@@ -26,15 +26,16 @@ class Antilink:
         self.regex = re.compile(r"<?(https?:\/\/)?(www\.)?(discord\.gg|discordapp\.com\/invite)\b([-a-zA-Z0-9/]*)>?")
         self.regex_discordme = re.compile(r"<?(https?:\/\/)?(www\.)?(discord\.me\/)\b([-a-zA-Z0-9/]*)>?")
 
-    @commands.group(pass_context=True, no_pm=True, hidden=True)
+    @commands.group(pass_context=True, no_pm=True)
+    @checks.is_admin()
     async def antilinkset(self, ctx):
         """Manages the settings for antilink."""
         serverid = ctx.message.guild.id
         if str(serverid) not in self.json:
             self.json[str(serverid)] = {'toggle': False, 'message': '', 'dm': False, 'ownerdm': False}
 
-    @antilinkset.command(pass_context=True, no_pm=True, hidden=True)
-    @checks.admin_or_permissions(administrator=True)
+    @antilinkset.command(pass_context=True, no_pm=True)
+    @checks.is_admin()
     async def ownerdm(self, ctx):
         """Enable/disables antilink owner dm in the server"""
         serverid = ctx.message.guild.id
@@ -46,8 +47,8 @@ class Antilink:
             await ctx.send('Owner DM now enabled')
         dataIO.save_json(self.location, self.json)
 
-    @antilinkset.command(pass_context=True, no_pm=True, hidden=True)
-    @checks.admin_or_permissions(administrator=True)
+    @antilinkset.command(pass_context=True, no_pm=True)
+    @checks.is_admin()
     async def toggle(self, ctx):
         """Enable/disables antilink in the server"""
         serverid = ctx.message.guild.id
@@ -59,8 +60,8 @@ class Antilink:
             await ctx.send('Antilink is now enabled')
         dataIO.save_json(self.location, self.json)
 
-    @antilinkset.command(pass_context=True, no_pm=True, hidden=True)
-    @checks.admin_or_permissions(administrator=True)
+    @antilinkset.command(pass_context=True, no_pm=True)
+    @checks.is_admin()
     async def message(self, ctx, *, text):
         """Set the message for when the user sends a illegal discord link"""
         serverid = ctx.message.guild.id
@@ -70,8 +71,8 @@ class Antilink:
         if self.json[str(serverid)]['dm'] is False:
             await ctx.send('Remember: Direct Messages on removal is disabled!\nEnable it with ``antilinkset toggledm``')
 
-    @antilinkset.command(pass_context=True, no_pm=True, hidden=True)
-    @checks.admin_or_permissions(administrator=True)
+    @antilinkset.command(pass_context=True, no_pm=True)
+    @checks.is_admin()
     async def toggledm(self, ctx):
         serverid = ctx.message.guild.id
         if self.json[str(serverid)]['dm'] is False:
