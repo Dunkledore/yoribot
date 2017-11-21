94d91d11587d86b23a79c99c9beeea8276e573c6
Dunkledore <b7dunkerley@me.com>
2017-11-21 01:51:17 +0000

    Revert "Revert "Revert "Revert "tidy the console on reboot""""
    This reverts commit 58206f366d24a9938fcd9dd32710aac7a4025f57.

--- cogs/discomegle.py
+++ cogs/discomegle.py
@@ -146,7 +146,10 @@ class Discomegle:
                 em = discord.Embed(description="**You have been paired. You can now start talking with your partner.**", colour=self.colour)
                 await user_two_channel.send(embed = em)  
 
-            await asyncio.sleep(5)
+                try:
+                    await asyncio.sleep(5)
+                except:
+                    pass
 
 def setup(bot):
     n = Discomegle(bot)
