09a1640f3da4fffe3a07510fe97d78767ea654eb
Dunkledore <b7dunkerley@me.com>
2017-11-25 18:47:23 +0000

    syntax issue

--- cogs/profile.py
+++ cogs/profile.py
@@ -32,24 +32,24 @@ class Profile:
 		await ctx.send(embed=em)
 
 	
-    def get_bot_uptime(self, *, brief=False):
-        now = datetime.datetime.utcnow()
-        delta = now - self.bot.uptime
-        hours, remainder = divmod(int(delta.total_seconds()), 3600)
-        minutes, seconds = divmod(remainder, 60)
-        days, hours = divmod(hours, 24)
+	def get_bot_uptime(self, *, brief=False):
+		now = datetime.datetime.utcnow()
+		delta = now - self.bot.uptime
+		hours, remainder = divmod(int(delta.total_seconds()), 3600)
+		minutes, seconds = divmod(remainder, 60)
+		days, hours = divmod(hours, 24)
 
-        if not brief:
-            if days:
-                fmt = '{d} days, {h} hours, {m} minutes, and {s} seconds'
-            else:
-                fmt = '{h} hours, {m} minutes, and {s} seconds'
-        else:
-            fmt = '{h}h {m}m {s}s'
-            if days:
-                fmt = '{d}d ' + fmt
+		if not brief:
+			if days:
+				 fmt = '{d} days, {h} hours, {m} minutes, and {s} seconds'
+		else:
+			fmt = '{h} hours, {m} minutes, and {s} seconds'
+		else:
+			fmt = '{h}h {m}m {s}s'
+			if days:
+				fmt = '{d}d ' + fmt
 
-        return fmt.format(d=days, h=hours, m=minutes, s=seconds)
+		  return fmt.format(d=days, h=hours, m=minutes, s=seconds)
 
 	async def sendYori(self, ctx):
 
