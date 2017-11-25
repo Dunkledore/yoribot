77061a47b77118d135c2bf0b8e52fabae1188829
Seneth <eadesjames@me.com>
2017-11-25 00:57:23 -0500

    fortune

--- cogs/searches.py
+++ cogs/searches.py
@@ -95,6 +95,20 @@ class Searches:
         await ctx.send("Step 3 - Click the Button")
         await asyncio.sleep(2)
         await ctx.send("That's it! https://www.google.com/search?q="+query)
+    
+    @commands.command(pass_context=True, no_pm=True)
+    async def getfortune(self, ctx):
+        """What is your fortune? Well then, lets find out..."""
+        
+        user = ctx.message.author
+        page = randint(1,6)
+        link = "http://fortunecookieapi.herokuapp.com/v1/fortunes?limit=&skip=&page={}".format(page)
+        async with aiohttp.get(link) as m:
+            result = await m.json()
+            message = choice(result)
+            fortune = discord.Embed(colour=user.colour)
+            fortune.add_field(name="{}'s Fortune!".format(user.display_name),value="{}".format(message["message"]))
+            await ctx.send(embed=fortune)
 
 def setup(bot):
     n = Searches(bot)
