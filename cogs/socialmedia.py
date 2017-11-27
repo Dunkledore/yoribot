e97525e4424fcd5cd6d9df5c862c77424543fb78
Dunkledore <b7dunkerley@me.com>
2017-11-27 19:36:44 +0000

    author

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -71,16 +71,16 @@ class SocialMedia:
 		async def check(m):
 			return m.author == ctx.author
 
-		await ctx.user.send("Please send your cosumer key")
+		await ctx.author.send("Please send your cosumer key")
 		consumer_key = await self.bot.wait_for('message', check=check, timeout=30.0)
 
-		await ctx.user.send("Please send your consumer secret")
+		await ctx.author.send("Please send your consumer secret")
 		consumer_secret = await self.bot.wait_for('message', check=check, timeout=30.0)
 
-		await ctx.user.send("Please send your access token")
+		await ctx.author.send("Please send your access token")
 		access_token = await self.bot.wait_for('message', check=check, timeout=30.0)
 
-		await ctx.user.send("Please send your access token secret")
+		await ctx.author.send("Please send your access token secret")
 		access_token_secret = await self.bot.wait_for('message', check=check, timeout=30.0)
 
 		insertquery = "INSERT INTO social_config (guild_id, twitter) VALUES ($1, $2)"
@@ -92,7 +92,7 @@ class SocialMedia:
 			await ctx.db.execute(insertquery, ctx.guild.id, twitter_creds)
 		except asyncpg.UniqueViolationError:
 			await ctx.db.execute(alterquery, ctx.guild.id, twitter_creds)
-		await ctx.send('Details Saved')
+		await ctx.author.send('Details Saved')
 
 
 	@commands.command(no_pm=True)
