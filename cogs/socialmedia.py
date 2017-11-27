a5598dc0c206232eb207e0de205fcf3ec6cf06ad
Dunkledore <b7dunkerley@me.com>
2017-11-27 22:48:17 +0000

    order issue

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -47,8 +47,8 @@ class SocialMedia:
 			else:
 				tweeter_role = results[0]["tweeter_role_id"]
 				tweeter_number = results[0]["tweeter_reaction"]
-			msg = box("Reactions until tweet: {1} 🐦\n"
-					  "Tweeter role ID:  {0}\n"
+			msg = box("Reactions until tweet: {0} 🐦\n"
+					  "Tweeter role ID:  {1}\n"
 					  .format(tweeter_number or "0", tweeter_role or "0"))
 			msg += "\n {}twittetset tweeter <role>".format(ctx.prefix)
 			msg += "\n {}twitterset tweetnumber <number>".format(ctx.prefix)
