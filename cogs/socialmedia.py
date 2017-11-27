ea4e8015b9240404bc1a2c3628a4f571606140af
Dunkledore <b7dunkerley@me.com>
2017-11-27 22:52:48 +0000

    different check

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -43,7 +43,7 @@ class SocialMedia:
 			results = await self.bot.pool.fetch(query, ctx.guild.id)
 			if results is None:
 				tweeter_role, tweeter_number = 0
-			elif results[0] is None:
+			elif len(results) == 0:
 				tweeter_role, tweeter_number = 0
 			else:
 				tweeter_role = results[0]["tweeter_role_id"]
