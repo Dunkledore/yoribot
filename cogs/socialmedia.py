067c5fd3a7e87e84a700b2b8e8cd090aa7cc41a4
Dunkledore <b7dunkerley@me.com>
2017-11-27 19:43:37 +0000

    message to content

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -73,19 +73,23 @@ class SocialMedia:
 
 		await ctx.author.send("Please send your cosumer key")
 		consumer_key = await self.bot.wait_for('message', check=check, timeout=30.0)
+		consumer_key = consumer_key.content
 		await asyncio.sleep(1)
 
 		await ctx.author.send("Please send your consumer secret")
 		consumer_secret = await self.bot.wait_for('message', check=check, timeout=30.0)
+		consumer_secret = consumer_key.content
 		await asyncio.sleep(1)
 
 		await ctx.author.send("Please send your access token")
 		access_token = await self.bot.wait_for('message', check=check, timeout=30.0)
+		access_token = access_token.content
 		await asyncio.sleep(1)
 
 		await ctx.author.send("Please send your access token secret")
 		access_token_secret = await self.bot.wait_for('message', check=check, timeout=30.0)
-
+		access_token_secret = access_token_secret.content
+		
 		insertquery = "INSERT INTO social_config (guild_id, twitter) VALUES ($1, $2)"
 		alterquery = "UPDATE  SET twitter = $2 WHERE guild_id = $1"
 
