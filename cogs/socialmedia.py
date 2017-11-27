df5ff28d130e64718609295b20283c3245fc046a
Dunkledore <b7dunkerley@me.com>
2017-11-27 20:41:21 +0000

    check

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -59,6 +59,7 @@ class SocialMedia:
 		insertquery = "INSERT INTO social_config (guild_id, tweeter_role_id) VALUES ($1, $2)"
 		alterquery = "UPDATE social_config SET tweeter_role_id = $2 WHERE guild_id = $1"
 		role_id = str(role.id)
+		await ctx.send(role_id)
 		try:
 			await ctx.db.execute(insertquery, ctx.guild.id, role_id)
 		except asyncpg.UniqueViolationError:
