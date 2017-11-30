183d30bfa57658f0a7e30f01f81e36879238b906
Dunkledore <b7dunkerley@me.com>
2017-11-30 02:19:36 +0000

    corrections

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -70,9 +70,7 @@ class SocialMedia:
 					except asyncpg.UniqueViolationError:
 						await self.bot.pool.execute(alterquery, self.bot.get_guild(result["guild_id"]), tweet_id)
 			except Exception as e:
-				exc_type, exc_obj, exc_tb = sys.exc_info()
-				fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-				print(exc_type, fname, exc_tb.tb_lineno)
+				print(e)
 			await asyncio.sleep(30)
 
 
