c4d1cf5ced9d6c260a8c0761565fde785b8d0f45
Dunkledore <b7dunkerley@me.com>
2017-11-30 02:17:09 +0000

    syntax

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -71,8 +71,8 @@ class SocialMedia:
 						await self.bot.pool.execute(alterquery, self.bot.get_guild(result["guild_id"]), tweet_id)
 			except Exception as e:
 				exc_type, exc_obj, exc_tb = sys.exc_info()
-    			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-   				print(exc_type, fname, exc_tb.tb_lineno)
+				fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+				print(exc_type, fname, exc_tb.tb_lineno)
 			await asyncio.sleep(30)
 
 
