98ddaec597f19eb6fb5179d5b13129ab2a8000ec
Dunkledore <b7dunkerley@me.com>
2017-12-02 16:58:37 +0000

    see traceback

--- cogs/rolemanager.py
+++ cogs/rolemanager.py
@@ -207,7 +207,7 @@ class RoleManager:
                 em = discord.Embed(color=ctx.message.author.color, description="An error occured removing the role")
                 em.set_author(name=e.__class__.__name__, icon_url="http://bit.ly/2qlsl5I")
                 await ctx.send(embed=em)
-                print (dir(e.__traceback__))
+                print (e.__traceback__)
 
     def save_settings(self):
         dataIO.save_json("data/rolemanager/settings.json", self.settings)
