471719dbe060219ab8bc07e411324495c3b9b1b2
Seneth <eadesjames@me.com>
2017-12-04 19:10:01 -0500

    SPACE CADET SENETH

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -127,11 +127,11 @@ class Welcome:
 	@checks.is_admin()
 	async def welcomewhisper(self, ctx):
 		altertquery = "UPDATE welcome_config SET whisper = NOT whisper WHERE guild_id = $1 RETURNING whisper"
-        insertquery = "INSERT INTO welcome_config (guild_id, whisper) VALUES ($1,True)"
-        try:
-        	await ctx.db.execute(insertquery, ctx.guild.id)
-        	await ctx.send("Whisper set to True")
-        except asuncpg.UniqueViolationError:
+		insertquery = "INSERT INTO welcome_config (guild_id, whisper) VALUES ($1,True)"
+		try:
+			await ctx.db.execute(insertquery, ctx.guild.id)
+			await ctx.send("Whisper set to True")
+		except asuncpg.UniqueViolationError:
 			whisper = await ctx.db.fetchval(query, ctx.guild.id)
 			await ctx.send("Whisper set to " + str(whisper))
 
