534770f6fb57009d8554762e90d74533a6f7f079
Seneth <eadesjames@me.com>
2017-12-09 22:02:46 -0500

    ctx?

--- cogs/away.py
+++ cogs/away.py
@@ -45,7 +45,7 @@ class Away:
             msg = 'You\'re now back.'
         else:
             self.data[ctx.message.author.id] = {}
-            message = message.content
+            message = ctx.message.content
             if len(message) < 256:
                 self.data[ctx.message.author.id]['MESSAGE'] = ' '.join(ctx.message.clean_content.split()[1:])
             else:
