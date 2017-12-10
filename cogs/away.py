efadfd1816fd133b11d9929e7bcb22a03379984b
Seneth <eadesjames@me.com>
2017-12-09 21:51:47 -0500

    int?

--- cogs/away.py
+++ cogs/away.py
@@ -19,19 +19,19 @@ class Away:
                 tmp[mention] = True
             if message.author.id != self.bot.user.id:
                 for author in tmp:
-                    if str(author.id) in self.data:
+                    if int(author.id) in self.data:
                         try:
                             avatar = author.avatar_url if author.avatar else author.default_avatar_url
-                            if self.data[str(author.id)]['MESSAGE']:
-                                em = discord.Embed(description=self.data[str(author.id)]['MESSAGE'], color=discord.Color.blue())
+                            if self.data[int(author.id)]['MESSAGE']:
+                                em = discord.Embed(description=self.data[int(author.id)]['MESSAGE'], color=discord.Color.blue())
                                 em.set_author(name='{} is currently away'.format(author.display_name), icon_url=avatar)
                             else:
                                 em = discord.Embed(color=discord.Color.blue())
                                 em.set_author(name='{} is currently away'.format(author.display_name), icon_url=avatar)
                             await self.bot.send_message(message.channel, embed=em)
                         except:
-                            if self.data[str(author.id)]['MESSAGE']:
-                                msg = '{} is currently away and has set the following message: `{}`'.format(author.display_name, self.data[str(author.id)]['MESSAGE'])
+                            if self.data[int(author.id)]['MESSAGE']:
+                                msg = '{} is currently away and has set the following message: `{}`'.format(author.display_name, self.data[int(author.id)]['MESSAGE'])
                             else:
                                 msg = '{} is currently away'.format(author.display_name)
                             await self.bot.send_message(message.channel, msg)
@@ -40,12 +40,12 @@ class Away:
     async def _away(self,ctx, *message: str):
         """Tell the bot you're away or back."""
         author = ctx.message.author
-        if str(author.id) in self.data:
-            del self.data[str(author.id)]
+        if int(author.id) in self.data:
+            del self.data[int(author.id)]
             msg = 'You\'re now back.'
         else:
             self.data[ctx.message.author.id] = {}
-            if len(str(message)) < 256:
+            if len(int(message)) < 256:
                 self.data[ctx.message.author.id]['MESSAGE'] = ' '.join(ctx.message.clean_content.split()[1:])
             else:
                 self.data[ctx.message.author.id]['MESSAGE'] = True
@@ -57,11 +57,11 @@ class Away:
     @checks.is_mod()
     async def _ignore(self,ctx):
         guild = ctx.message.guild
-        if str(guild.id) in self.data:
-            del self.data[str(guild.id)]
+        if int(guild.id) in self.data:
+            del self.data[int(guild.id)]
             message = 'Not ignoring this guild anymore.'
         else:
-            self.data[str(guild.id)] = True
+            self.data[int(guild.id)] = True
             message = 'Ignoring this guild.'
         dataIO.save_json('data/away/away.json', self.data)
         await ctx.send(message)
