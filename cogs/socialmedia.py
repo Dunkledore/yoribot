f2a5e007e0a0addedafe22076e62ce4595ab7710
Dunkledore <b7dunkerley@me.com>
2017-12-17 03:00:16 +0000

    error

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -79,7 +79,7 @@ class SocialMedia:
 					except asyncpg.UniqueViolationError:
 						await self.bot.pool.execute(alterquery, result["guild_id"], tweet_id)
 		except Exception as e:
-			print(e)
+			print('error')
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
 			print(exc_type, fname, exc_tb.tb_lineno)
