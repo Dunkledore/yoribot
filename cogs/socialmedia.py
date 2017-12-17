56b991da4982ef617bf85b50a34abcceda78db0a
Dunkledore <b7dunkerley@me.com>
2017-12-17 03:01:55 +0000

    print all

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -47,42 +47,37 @@ class SocialMedia:
 
 
 	async def feeds(self):
-		try:
-			while True:
-				query = "SELECT guild_id, feed_channel, last_tweet FROM social_config"
-				results = await self.bot.pool.fetch(query)
-				
-				for result in results:
-					if not result["feed_channel"]:
-						continue
-					creds = await self.get_creds(self.bot.get_guild(result["guild_id"]))
-					if not creds:
-						continue
-					api = self.get_api(creds)
-					me = api.me()
-					if not result["last_tweet"]:
-						tweets = api.user_timeline(id=me.id,count=20)
-					else:
-						tweets = api.user_timeline(id=me.id,since_id=result["last_tweet"])
+		while True:
+			query = "SELECT guild_id, feed_channel, last_tweet FROM social_config"
+			results = await self.bot.pool.fetch(query)
+			
+			for result in results:
+				if not result["feed_channel"]:
+					continue
+				creds = await self.get_creds(self.bot.get_guild(result["guild_id"]))
+				if not creds:
+					continue
+				api = self.get_api(creds)
+				me = api.me()
+				if not result["last_tweet"]:
+					tweets = api.user_timeline(id=me.id,count=20)
+				else:
+					tweets = api.user_timeline(id=me.id,since_id=result["last_tweet"])
 
-					tweet_id = result["last_tweet"]
-					for tweet in tweets:
-						channel = self.bot.get_channel(result["feed_channel"])
-						await channel.send(embed=self.tweetToEmbed(tweet))
-						tweet_id = tweet.id
+				tweet_id = result["last_tweet"]
+				for tweet in tweets:
+					channel = self.bot.get_channel(result["feed_channel"])
+					await channel.send(embed=self.tweetToEmbed(tweet))
+					tweet_id = tweet.id
 
-					insertquery = "INSERT INTO social_config (guild_id, last_tweet) VALUES ($1, $2)"
-					alterquery = "UPDATE social_config SET last_tweet = $2 WHERE guild_id = $1"
+				insertquery = "INSERT INTO social_config (guild_id, last_tweet) VALUES ($1, $2)"
+				alterquery = "UPDATE social_config SET last_tweet = $2 WHERE guild_id = $1"
 
-					try:
-						await self.bot.pool.execute(insertquery, result["guild_id"], tweet_id)
-					except asyncpg.UniqueViolationError:
-						await self.bot.pool.execute(alterquery, result["guild_id"], tweet_id)
-		except Exception as e:
-			print('error')
-			exc_type, exc_obj, exc_tb = sys.exc_info()
-			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-			print(exc_type, fname, exc_tb.tb_lineno)
+				try:
+					await self.bot.pool.execute(insertquery, result["guild_id"], tweet_id)
+				except asyncpg.UniqueViolationError:
+					await self.bot.pool.execute(alterquery, result["guild_id"], tweet_id)
+
 
 			await asyncio.sleep(30)
 
