08a41ca9c4d812fd2c8b97b12345a4a6d074c221
James <eadesjames@me.com>
2017-12-21 20:22:34 -0500

    Twitter Jazz

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -42,9 +42,10 @@ class SocialMedia:
 
 
 	def tweetToEmbed(self, tweet):
-		em = discord.Embed(color=discord.Colour.teal())
+		em = discord.Embed(title="Follow Us on Twitter", url=tweet.user.url ,color=discord.Colour.teal())
 		em.set_author(name=tweet.user.name, icon_url=tweet.user.profile_image_url)
 		em.add_field(name="Tweet", value=tweet.text)
+		em.set_footer(text="Followers:" + tweet.user.followers_count + "Following:" + tweet.user.friends_count + "Tweets:" + tweet.user.statuses_count)
 		if 'media' in tweet.entities:
 			if tweet.entities['media'][0]['type'] == 'photo':
 				em.set_image(url=tweet.entities['media'][0]['media_url_https'])
