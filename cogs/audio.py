ce39bb9141cca3bf3cba95c300839962e45835be
tweelix <34718410+tweelix@users.noreply.github.com>
2017-12-22 21:47:50 +0100

    autofront stuff

--- cogs/audio.py
+++ cogs/audio.py
@@ -11,7 +11,7 @@ class Music:
 
 	def __init__(self,bot):
 		self.bot = bot
-		self.counter=[]
+		self.counter={}
 
 	async def has_majority(self, reaction):
 		listeners = len(reaction.message.guild.voice_client.channel.members)
