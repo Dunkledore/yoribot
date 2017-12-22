18da0b35df5bc7508a16d6002481b8d3f2d99f7e
James <eadesjames@me.com>
2017-12-21 22:43:23 -0500

    trying this way

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -43,12 +43,11 @@ class SocialMedia:
 
 	def tweetToEmbed(self, tweet):
 		em = discord.Embed(title="Follow Us", url="http://twitter.com/"+tweet.user.screen_name, description=tweet.text, color=discord.Colour.teal())
-		em.set_author(name=tweet.user.name, icon_url="http://bit.ly/2p7n1Xw")
-		em.set_thumbnail(url=tweet.user.profile_image_url)
+		em.set_author(name=tweet.user.name, icon_url=tweet.user.profile_image_url)
 		em.add_field(name="Following:", value=str(tweet.user.friends_count)) 
 		em.add_field(name="Followers", value=str(tweet.user.followers_count))
 		em.add_field(name="Tweets:", value=str(tweet.user.statuses_count))
-		em.set_footer(text="Sent: "+ str(tweet.created_at))
+		em.set_footer(text="Sent: "+ str(tweet.created_at), icon_url="http://bit.ly/2p7n1Xw")
 		if 'media' in tweet.entities:
 			if tweet.entities['media'][0]['type'] == 'photo':
 				em.set_image(url=tweet.entities['media'][0]['media_url_https'])
