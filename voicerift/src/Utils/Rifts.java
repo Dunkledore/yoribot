29e6f839f58bc88fa4b8832e51ff3df43e59613f
Dunkledore <b7dunkerley@me.com>
2017-12-28 01:31:13 +0000

    voicerift

---
+++ voicerift/src/Utils/Rifts.java
@@ -0,0 +1,13 @@
+package Utils;
+
+import java.util.HashMap;
+
+public class Rifts
+{
+  public static HashMap<String, Rift> rifts;
+  
+  public Rifts() 
+  {
+    rifts = new HashMap<String, Rift>();
+  }
+}
