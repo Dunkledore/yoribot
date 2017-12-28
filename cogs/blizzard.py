eb30219465cc1e1ae385144b412cf7e3cb9ffd34
James <eadesjames@me.com>
2017-12-27 19:26:09 -0500

    ==

--- cogs/blizzard.py
+++ cogs/blizzard.py
@@ -88,10 +88,10 @@ class Blizzard:
             await message.add_reaction(str(emoji['next']))
 
         def check(reaction, user):
-            return reaction.message == message and user = ctx.message.author
+            return reaction.message == message and user == ctx.message.author
 
         r = await self.bot.wait_for('reaction', timeout=timeout)
-        
+
         if r is None:
             return [None, message]
 
