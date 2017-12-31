16cc9825b980192b95e701a5b3ef981c516433c0
Dunkledore <b7dunkerley@me.com>
2017-12-31 15:42:26 +0000

    object

--- cogs/mod.py
+++ cogs/mod.py
@@ -203,6 +203,7 @@ class Mod:
             if len(self.media_count[message.guild][message.author]) > 2:
                 if (self.media_count[message.guild][message.author][-1][3] - self.media_count[message.guild][message.author][-3][3]).total_seconds() < 10:
                     for item in self.media_count[message.guild][message.author][-1:-3]:
+                        await message.channel.send(str(item))
                         try:
                             await item[0].delete()
                         except Exception as e:
