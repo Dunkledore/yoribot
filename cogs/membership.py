264f2d20a49fbdfdf11bf7fce3c01e4f8ee378f7
Dunkledore <b7dunkerley@me.com>
2017-12-31 02:20:34 +0000

    checks

--- cogs/membership.py
+++ cogs/membership.py
@@ -87,8 +87,9 @@ class MemberAudit:
 	@checks.is_mod()
 	async def log(self, ctx, member: discord.Member, *, reason=None):
 		if reason:
+			await ctx.send('logging')
 			self._log(ctx, member, 'Note', reason)
-		await ctx.send(reason)
+
 		embed = await self.log_as_embed(member.id, ctx.guild.id)
 		await ctx.send(embed=embed)
 
@@ -98,6 +99,7 @@ class MemberAudit:
 
 		query = "INSERT INTO mod_log (guild_id, user_id, user_name, action, reason, mod_id, mod_name) VALUES ($1, $2, $3, $4, $5, $6, $7)"
 		await ctx.db.execute(query, ctx.guild.id, member.id, member.nick or member.name, 'Note', reason, ctx.author.id, ctx.author.name)
+		await ctx.send('in db')
 
 
 	@commands.command(no_pm=True)
