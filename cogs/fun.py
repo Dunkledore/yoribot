c850a7d9cd96d60469a9b675fa9f477137e67932
Dunkledore <b7dunkerley@me.com>
2018-01-01 21:43:09 +0000

    name

--- cogs/fun.py
+++ cogs/fun.py
@@ -208,7 +208,7 @@ class Fun:
         """Because everyone likes hugs
 
         Up to 10 intensity levels."""
-        name = user.display_name
+        name = user.nick or user.name
         if intensity <= 0:
             msg = "(っ˘̩╭╮˘̩)っ" + name
         elif intensity <= 3:
