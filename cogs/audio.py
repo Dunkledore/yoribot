011594ef303eebf3b2619278f63a728b76cf5614
James <eadesjames@me.com>
2018-01-08 13:22:45 -0500

    Trying

--- cogs/audio.py
+++ cogs/audio.py
@@ -4,7 +4,8 @@ from discord.ext import commands
 from .utils import checks, formats
 
 from .utils import datatools
-#from .music import _data, _musicplayer
+from .music import _data
+from .music import _musicplayer
 
 
 class Music:
