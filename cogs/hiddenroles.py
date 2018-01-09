9882a72938928c198c7e7e3da9955bf3dee290af
Ar4093 <>
2018-01-09 09:42:05 +0100

    testing command

--- cogs/hiddenroles.py
+++ cogs/hiddenroles.py
@@ -4,12 +4,14 @@ import asyncio
 import json
 from .utils import db, checks
 
-def embedHR(colour, description, footer):
+
+def embedHR(colour="green", description="", footer=""):
     colours = {"red": discord.Colour.red, "green": discord.Colour.green, "yellow": discord.Colour.gold}
     em = discord.Embed(color=colours[colour], description=description)
     em.set_footer(text=footer)
     em.set_author(name="Hidden Role Helper", icon_url="http://yoribot.com/wp-content/uploads/2017/11/yoriicon.png")
     return em
+
 
 class Hiddenroles:
     def __init__(self, bot: commands.Bot):
@@ -27,7 +29,7 @@ class Hiddenroles:
     @checks.is_mod()
     async def hiddenrole(self, ctx):
         prefix = self.bot.get_guild_prefixes(ctx.message.guild)[2]
-        em = embedHR("yellow", "This command group is used for management of hidden roles.")
+        em = embedHR("yellow", "This command group is used for management of hidden roles.\n"+prefix)
         await ctx.send(embed=em)
 
     @hiddenrole.command(name="create")
@@ -40,6 +42,7 @@ class Hiddenroles:
     @checks.is_developer()
     async def testing(self, ctx):
         await self._getdata(ctx)
+
 
 def setup(bot):
     bot.add_cog(Hiddenroles(bot))
