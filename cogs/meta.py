4bd60201b2e8e8da7497f2073500035cef714367
James <eadesjames@me.com>
2018-01-12 15:52:41 -0500

    ok fuck you purge

--- cogs/meta.py
+++ cogs/meta.py
@@ -450,7 +450,6 @@ class Meta:
                                       if y == 'percent'], key=lambda x: x[1])
         others = 100 - sum(x[1] for x in top_ten)
         img = self.create_chart(top_ten, others)
-        await channel.purge(limit=1, check=em)
         await ctx.send(file=discord.File(img))
 
 def check_folders():
