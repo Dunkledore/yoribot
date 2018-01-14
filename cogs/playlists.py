b3b6f9b2043a0ddb59225c30a2d5d68c8a497a18
Tweelix <felix.altenberg@outlook.com>
2018-01-14 16:12:47 +0000

    "

--- cogs/playlists.py
+++ cogs/playlists.py
@@ -65,7 +65,7 @@ class Playlists:
 	
 			
 	@commands.command()		
-	async def playlist(self,ctx,command,name,*inputs):
+	async def playlist(self,ctx,command,name,inputs):
 		self.context=ctx
 		await ctx.send("hi")
 		if command.lower() == 'add':
