be3904ac29bea277c10f75f6b0f39689a3656a10
Tweelix <felix.altenberg@outlook.com>
2018-01-15 17:39:47 +0000

    more playlist stuff

--- cogs/playlists.py
+++ cogs/playlists.py
@@ -1,8 +1,3 @@
-'''List of  possible errors:
-name too long limit 255
-playlist already exists cunt
-not youtube link lelelelell
-'''
 from .music import api_youtube
 from .utils import db, checks, formats, cache
 
@@ -48,12 +43,17 @@ class Playlists:
 		
 		if result[0]["songs"]:
 			self.list=self.convert_from_storage(result[0]["songs"])
+		return self.list
 		
 	async def save_playlist(self,userID,name):
 		await self.context.send("save called")
-		query = "UPDATE playlists SET Songs = $3 WHERE userid=$1 AND name = $2"
+		query = "UPDATE playlists SET Songs = $3, No_Songs = $4 WHERE userid=$1 AND name = $2"
 		songs=self.convert_to_storage(self.list)
-		await self.context.db.execute(query, userID, name, songs)
+		await self.context.db.execute(query, userID, name, songs, len(self.list))
+		
+		embed=discord.Embed(title="Playlist {} Successfully Updated!".format(name), colour=discord.Colour.blurple())
+		summary.add_field(name="No. of Videos:",value=len(self.list))
+		await ctx.channel.send(embed=embed)
 		
 		self.list=[]
 		
@@ -106,26 +106,29 @@ class Playlists:
 		await self.context.send("delete called")
 		query = "DELETE FROM playlists WHERE userid=$1 AND name=$2;"
 		await self.context.db.execute(query, userID, name)
-		#send message to say it's been deleted
+		
+		message="Playlist {} has been successfully deleted".format(name)
+		embed=discord.Embed(title="", colour=discord.Colour.blurple())
+		summary.add_field(name="Success!",value=message)
+		await self.context.channel.send(embed=embed)
 
 	async def send_help(self,ctx):
-		#creates and returns the embed summary of what the user will have missed
 		help=discord.Embed(title="", colour=discord.Colour.blurple())
-		prefix=ctx.prefix
-		summary.add_field(name="Invalid Syntax", ("Use \""+prefix+"help playlist\" for list of possible commands") )
-	
-		return help
-	
+		summary.add_field(name="Invalid Syntax",value = "Use \"{}help playlist\" for list of possible commands".format(ctx.prefix))
+		await ctx.channel.send(embed=help)
+		
+	async def send_list(self,ctx):
+		embed=discord.Embed(title="", colour=discord.Colour.blurple())
+		summary.add_field(name="ERROR",value=message)
+		await ctx.channel.send(embed=embed)
 			
 	@commands.command()
-	async def playlist(self,ctx,command,name,*inputs):
-		await ctx.send("hi")
+	async def playlist(self,ctx,command,playlistname,*inputs):
 		self.context=ctx
 		inputs=list(inputs)
 		if command.lower() == 'add':
-			await ctx.send("command:add")
-			if not await self.playlist_exists(ctx.message.author.id,name):
-				await self.create_playlist(ctx.message.author.id,name)
+			if not await self.playlist_exists(ctx.message.author.id,playlistname):
+				await self.create_playlist(ctx.message.author.id,playlistname)
 			
 			front=False
 			if 'front' in inputs:
@@ -133,24 +136,45 @@ class Playlists:
 				inputs = inputs[:-1]
 			for item in inputs:
 				if not self.check_query(item):
+					#Error message sending#
+					message="{} is not a valid input. Valid inputs are Youtube video and Playlist URLs".format(item)
+					embed=discord.Embed(title="", colour=discord.Colour.blurple())
+					summary.add_field(name="ERROR",value=message)
+					await ctx.channel.send(embed=embed)
+					#######################
 					continue
-				await self.add_to_playlist(ctx.message.author.id,name,item,front)
+				await self.add_to_playlist(ctx.message.author.id,playlistname,item,front)
 			await self.context.send(str(self.list))
 		
 		elif command.lower() == 'remove':
-			if not await self.playlist_exists(ctx.message.author.id,name):
+			if not await self.playlist_exists(ctx.message.author.id,playlistname):
 				say doesnt exists
 				return
 			for item in inputs:
 				if not self.check_query(item):
+					#Error message sending#
+					message="{} is not a valid input. Valid inputs are Youtube video and Playlist URLs".format(item)
+					embed=discord.Embed(title="", colour=discord.Colour.blurple())
+					summary.add_field(name="ERROR",value=message)
+					await ctx.channel.send(embed=embed)
+					#######################
 					continue
-				await self.remove_from_playlist(ctx.message.author.id,name,item)
+				await self.remove_from_playlist(ctx.message.author.id,playlistname,item)
 		
 		elif command.lower() == 'delete':
-			if not await self.playlist_exists(ctx.message.author.id,name):
-				await ctx.send("doesn't exists")
+			if not await self.playlist_exists(ctx.message.author.id,playlistname):
+				#Error message sending#
+				message="Playlist \"{}\" does not exist".format(playlistname)
+				embed=discord.Embed(title="", colour=discord.Colour.blurple())
+				summary.add_field(name="ERROR",value=message)
+				await ctx.channel.send(embed=embed)
+				#######################
 				return
-			await self.delete_playlist(ctx.message.author.id,name)
+			await self.delete_playlist(ctx.message.author.id,playlistname)
+		elif command.lower() == 'list':
+			await self.send_list(ctx)
+		else:
+			await self.send_help(ctx)
 
 	
 def setup(bot):
