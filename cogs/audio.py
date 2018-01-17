7740053267c1ac8e78e12318e5d7a0ce46c0da09
Tweelix <felix.altenberg@outlook.com>
2018-01-17 15:05:20 +0000

    await was the problem

--- cogs/audio.py
+++ cogs/audio.py
@@ -94,7 +94,7 @@ class Music:
 			
 		await ctx.message.delete()
 		if await playlists.playlist_exists(ctx,ctx.message.author.id,query):
-			list=playlists.get_playlist(ctx,ctx.message.author.id,query)
+			list= await playlists.get_playlist(ctx,ctx.message.author.id,query)
 			playlistinfo={"name":query, "author":ctx.message.author, "list":list}
 			await self.getMusicPlayer(str(ctx.guild.id)).play(ctx.author, ctx.channel, query,False,False, playlistinfo)
 		else:
