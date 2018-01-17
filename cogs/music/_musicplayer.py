5ccfb260f8aaf8c7c4ada7c3fa524f7a62a4d573
Tweelix <felix.altenberg@outlook.com>
2018-01-17 13:21:17 +0000

    nevermind

--- cogs/music/_musicplayer.py
+++ cogs/music/_musicplayer.py
@@ -584,22 +584,19 @@ class MusicPlayer:
 
 			self.queue.pop(0)
 			
-			try:
-				player = await YTDLSource.from_url(song, loop=self.bot.loop)
-				self.streamer = player
-				self.state = "ready"
+			player = await YTDLSource.from_url(song, loop=self.bot.loop)
+			self.streamer = player
+			self.state = "ready"
 
-				self.streamer.volume = self.volume / 100
-				self.vclient.play(player, after=vafter_inside)
+			self.streamer.volume = self.volume / 100
+			self.vclient.play(player, after=vafter_inside)
 
-				self.statuslog.info("Playing")
-				self.nowplayinglog.info(songname)
+			self.statuslog.info("Playing")
+			self.nowplayinglog.info(songname)
 
-				await self.embed.sent_embed.clear_reactions()
-				await self.add_reactions(False)
-			except:
-				self.state="ready"
-				self.skip()
+			await self.embed.sent_embed.clear_reactions()
+			await self.add_reactions(False)
+
 
 		else:
 			self.statuslog.info("Finished queue")
