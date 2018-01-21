f3bfe9d16c899968813ab7ca6c1216963f014715
Dunkledore <b7dunkerley@me.com>
2018-01-21 10:01:27 +0000

    ?

--- cogs/profile.py
+++ cogs/profile.py
@@ -79,7 +79,7 @@ class Rank:
 		for rank in ranks:
 			role = discord.utils.get(ctx.guild.roles, id=rank["role_id"])
 			if role:
-				humanranks.append(list(role.mention, rank["xp_required"]))
+				humanranks.append(list((role.mention, rank["xp_required"])))
 			else:
 				humanranks.append(["@deleted_role"],["xp_required"])
 		await ctx.send(str(humanranks))
