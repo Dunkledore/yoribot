766002a017d8e7c9646548ee292d9a0e547880cd
Dunkledore <b7dunkerley@me.com>
2018-01-22 11:20:38 +0000

    see this

--- cogs/profile.py
+++ cogs/profile.py
@@ -130,6 +130,7 @@ class Rank:
 
         entries = {}
         for member in ordered_guild_data_list:
+            await ctx.send(member)
             text = "Guild_XP: " + member[1][str(ctx.guild.id)] + "\n" + "Global: " + member[1]["Global"]
             entries[self.bot.get_user(int(member[0])).name] = text
         await ctx.send(entries)
