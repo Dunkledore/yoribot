1111f0f4137128d52996a0dbb1282fe738d01194
Dunkledore <b7dunkerley@me.com>
2018-01-23 18:36:20 +0000

    that autocomplete sb3...

--- cogs/mod.py
+++ cogs/mod.py
@@ -1105,7 +1105,7 @@ class Mod:
         invites = [i for i in all_invites if i.uses <= uses and i.created_at < (datetime.utcnow() - timedelta(hours=1))]
 
         if not invites:
-            return ctx.safe_mention_channel_ids('I didn\'t find any invites matching your criteria')
+            return ctx.send('I didn\'t find any invites matching your criteria')
 
         message = await ctx.send('Ok, a total of {} invites created by {} users with {} total uses would be pruned.'.format(
                 len(invites),
