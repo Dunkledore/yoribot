5fb5446518446c2d236a4635eef16508fb519b6e
Dunkledore <b7dunkerley@me.com>
2018-01-24 08:47:08 +0000

    more testing

--- cogs/rift.py
+++ cogs/rift.py
@@ -355,6 +355,9 @@ class Rift:
         prefs = prefixes = tuple(self.bot.get_guild_prefixes(msg.guild))
         for p in prefs:
             if msg.content.startswith(p):
+                await msg.channel.send(msg.content[len(p):].lower().split(" ")[0])
+                await msg.channel.send(self.bot.commands[0])
+                await msg.channel.send(self.bot.commands)
                 if msg.content[len(p):].lower().split(" ")[0] in self.bot.commands:
                     return
         orift = {k:v for k,v in self.open_rifts.items() if v}
