d35e0f4bb35cd69bd8a9689cad7b604a9067cf4f
James <eadesjames@me.com>
2018-02-19 13:20:07 -0500

    get user user

--- cogs/memberaudit.py
+++ cogs/memberaudit.py
@@ -432,7 +432,7 @@ class MemberAudit:
 
 	async def hub_ban_audit(self,guild,user: discord.User):
 		server = guild
-		user = self.bot.get_user(member)
+		user = self.bot.get_user(user)
 		hubchannel=self.bot.get_channel(381089479450034176)
 		embed = discord.Embed(title= user.name + ' WAS BANNED ', colour=discord.Colour.red())
 		embed.set_author(name= "🔨 User Action Report",)
