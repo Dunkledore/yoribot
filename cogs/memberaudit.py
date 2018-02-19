91ed30a0810aa4a2352070c421479c4ac53409aa
James <eadesjames@me.com>
2018-02-19 14:31:52 -0500

    Log - can I break it?

--- cogs/memberaudit.py
+++ cogs/memberaudit.py
@@ -441,13 +441,16 @@ class MemberAudit:
 						bannedin += guild.name + '\n'
 			except Exception as e:
 				pass
+		reason = discord.utils.get(bans, user=user)[0]
 		hubchannel=self.bot.get_channel(381089479450034176)
 		embed = discord.Embed(title= "User Name: " + str(user.name) + " User ID: " + str(user.id),  colour=discord.Colour.red())
 		embed.set_author(name= "🔨 User Action Report for " + str(user.name), icon_url=server.icon_url)
 		embed.add_field(name= "Server:", value= server.name)
 		embed.add_field(name= "Server ID: ", value = str(server.id))
-		embed.add_field(name= "Proof: ", value= " Coming soon")
+		embed.add_field(name= "Reason: ", value= reason)
+		embed.add_field(name="Proof:", value = "Coming Soon")
 		embed.set_thumbnail(url=user.avatar_url)
+
 		if bannedin:
 			embed.add_field(name='Banned In', value = bannedin, inline=False)
 		await hubchannel.send(embed=embed)
