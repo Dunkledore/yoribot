ed9b8dd0a0c68e258367542e919d51a6743831d4
James <eadesjames@me.com>
2018-02-19 13:54:22 -0500

    Jet Fuel

--- cogs/memberaudit.py
+++ cogs/memberaudit.py
@@ -434,7 +434,7 @@ class MemberAudit:
 		server = guild
 		hubchannel=self.bot.get_channel(381089479450034176)
 		embed = discord.Embed(title= "User ID: " + str(user.id) + "User Name: " + str(user.name), colour=discord.Colour.red())
-		embed.set_author(name= "🔨 User Action Report", icon_url=user.avatar_url)
+		embed.set_author(name= "🔨 User Action Report", icon_url=server.icon_url)
 		await hubchannel.send(embed=embed)
 
 	async def member_unban(self, guild, user: discord.User):
