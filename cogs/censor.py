c7515dd5afaee86664964b12ef26f2be1da1e78f
Dunkledore <b7dunkerley@me.com>
2018-02-27 17:05:02 +0000

    sighs

--- cogs/censor.py
+++ cogs/censor.py
@@ -271,7 +271,7 @@ class Censor:
             for key in [ALL_CHANNELS, message.channel.id]:
                 if key in self.regexen[sid]:
                     patterns.update(self.regexen[sid][key])
-            await message.channe.send(str(patterns))
+            await message.channel.send(str(patterns))
             # Iterate through patterns
             for regex, mode in patterns.items():
                 # Skip disabled patterns
