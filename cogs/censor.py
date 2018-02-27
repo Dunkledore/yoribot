19bb1a30234ce569694f42d803553b5a0bba9df9
Dunkledore <b7dunkerley@me.com>
2018-02-27 18:43:27 +0000

    ok tidy time

--- cogs/censor.py
+++ cogs/censor.py
@@ -80,13 +80,8 @@ class Censor:
                 self.recache[regex] = re.compile(regex)
 
     @commands.command()
-    async def censortest(self, ctx, test):
-        self.on_message(ctx.message)
-
-    @commands.command()
     async def censorlist(self, ctx, channel: discord.TextChannel= None):
-        """Lists regexes used to filter messages.
-        Channel listing includes global patterns."""
+        """Channel listing includes global patterns."""
         guild = ctx.message.guild
         self.regexen = dataIO.load_json(JSON_PATH)
         if not self._re_present(guild):
@@ -111,18 +106,8 @@ class Censor:
         await ctx.send('```py\n' + table + '```')
 
     @commands.command()
-    async def censoradd(self, ctx, pattern: str, mode, channel: discord.TextChannel):
-        """Adds a pattern to filter messages. Mods, bot admins, and the bot's
-        owner are not subjected to the filter.
-        If the pattern contains spaces, it must be put in double quotes. Single quotes will not work.
-
-        mode is one of:
-        incl: Default, filter messages that match the pattern
-        excl: filter non-matching, only one allowed per channel or guild
-        none: adds pattern to storage but doesn't apply filtering. Use recensor set to enable.
-
-        To use channel, mode must also be specified. If channel is not specified,
-        the filter is used across the entire guild."""
+    async def censoradd(self, ctx, pattern: str, mode, channel: discord.TextChannel = None:
+        """Adds a pattern to filter messages. Modes avaliable are incl or excl"""
         guild = ctx.message.guild
 
         # initialize
@@ -151,62 +136,10 @@ class Censor:
         await ctx.send('Pattern added.')
         dataIO.save_json(JSON_PATH, self.regexen)
 
-    @commands.command()
-    async def censorset(self, ctx, mode, channel):
-        """Lists regexes used to filter messages"""
-        guild = ctx.message.guild
-        self.regexen = dataIO.load_json(JSON_PATH)
-        if not self._re_present(guild):
-            await ctx.send('There are no patterns in the guild to modify.')
-            return
-        if mode not in MODES:
-            self.bot.reply('"%s" is not a valid mode. You must specify one of `%s`.') % (
-                mode, '`, `'.join(MODES))
-
-        if mode == MODE_EXCLUSIVE:
-            if ALL_CHANNELS in self._ls_excl(guild):
-                await ctx.send("There is already a guild-wide exclusive filter. Remove or disable it first.")
-                return
-            if channel and str(channel.id) in self._ls_excl(guild):
-                await ctx.send("That channel already has an exclusive filter. Remove or disable it first.")
-                return
-
-        re_list = {}
-        i = 1
-        table = ' | '.join(['#'.ljust(4), 'mode', 'pattern']) + '\n'  # header
-        for c in self.regexen[str(guild.id)]:
-            if c == ALL_CHANNELS and self._re_present(guild):
-                table += '\nguild-wide:\n'
-            elif (channel and str(channel.id) == c) or not channel:
-                if channel:
-                    ch_obj = channel
-                else:
-                    ch_obj = self.bot.get_channel(c)
-                if ch_obj is None:
-                    table += '\n' + 'Channel ID %s (deleted):' % c + '\n'
-                if self._re_present(ch_obj):
-                    table += '\n#' + ch_obj.name + '\n'
-
-            for regex, oldmode in self.regexen[str(guild.id)][c].items():
-                table += ' | '.join([str(i).ljust(4), oldmode, regex]) + '\n'
-                re_list[str(i)] = (str(guild.id), c, regex, oldmode)
-                i += 1
-        prompt = 'Choose the number of the pattern to set to `%s`:\n' % mode
-        await ctx.send(prompt + '```py\n' + table + '```')
-
-        msg = await self.bot.wait_for_message(author=ctx.message.author, timeout=15)
-        if msg is None:
-            return
-        msg = msg.content.strip()
-        if msg in re_list:
-            sid, cid, regex, _ = re_list[msg]
-            self.regexen[sid][cid][regex] = mode
-            await ctx.send('Mode set.')
-            dataIO.save_json(JSON_PATH, self.regexen)
 
     @commands.command()
     async def censordelete(self, ctx, channel):
-        """Lists regexes used to filter messages"""
+        """Deletes a pattern from the list"""
         guild = ctx.message.guild
         self.regexen = dataIO.load_json(JSON_PATH)
         if not self._re_present(guild):
@@ -281,10 +214,6 @@ class Censor:
 
     async def on_message_edit(self, old_message, new_message):
         await self.on_message(new_message)
-
-    async def on_command(self, command, ctx):
-        if ctx.cog is self:
-            self.analytics.command(ctx)
 
 
 def check_folder():
