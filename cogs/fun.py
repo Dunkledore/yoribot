9a7af4891674afa6495c0d90fa39253a0d558936
Jordan Menard <jordanmkasla2009@gmail.com>
2018-03-02 22:41:53 -0500

    :lipstick:

--- cogs/fun.py
+++ cogs/fun.py
@@ -257,7 +257,7 @@ class Fun:
 
     @commands.command()
     async def blame(self, ctx, user: discord.Member):
-        embed = discord.Embed(color=ctx.message.author.color, title="Blame this person",
+        embed = discord.Embed(color=ctx.message.author.color, title="❕ Blame this person ➡️",
                               description=user.display_name)
         embed.set_thumbnail(url=user.avatar_url)
         await ctx.send(embed=embed)
