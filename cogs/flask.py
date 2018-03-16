a6b8e754a7f5e674e1792fd8dfaf4e6532ac1c87
Dunkledore <b7dunkerley@me.com>
2018-03-16 14:40:31 +0000

    I was calling them the same name

--- cogs/flask.py
+++ cogs/flask.py
@@ -3,21 +3,18 @@ from discord.ext import commands
 from flask import Flask, request, render_template
 from .utils import checks
 
-class Flask:
+class Website:
 	"""The Welcome Related Commands"""
 
-	def __init__(self, bot):
+	def __init__(self, bot, app):
 		self.bot = bot
-		self.app = None
-
-	@commands.command()
-	@checks.is_developer()
-	async def make_app(self, ctx):
 		self.app = Flask(__name__)
 
+		
 	@commands.command()
 	@checks.is_developer()
 	async def run_app(self, ctx):
+		self.app.add_url_rule('/', 'index,', self.index)
 		self.app.run()
 
 
@@ -25,7 +22,8 @@ class Flask:
 		return render_template('index.html')
 
 def setup(bot):
-    bot.add_cog(Flask(bot))
+    bot.add_cog(Website(bot))
+
 
 
 
