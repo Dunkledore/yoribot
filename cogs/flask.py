9730c14280a08e4e1a476f442e927870134876e1
Dunkledore <b7dunkerley@me.com>
2018-03-16 13:45:30 +0000

    trying flask

---
+++ cogs/flask.py
@@ -0,0 +1,46 @@
+
+
+class Flask:
+	"""The Welcome Related Commands"""
+
+	def __init__(self, bot: commands.Bot):
+		self.bot = bot
+		self.app = None
+
+	@commands.command()
+	@check.is_developer()
+	async def run_app(self, ctx):
+		app.run()
+
+
+	@commands.command()
+	@checks.is_developer()
+	async def build_app(self, ctx):
+		from flask import Flask, request, render_template
+
+		self.app = Flask(__name__)
+
+		@self.app.route('/form')
+		def my_form():
+		    return render_template('my-form.html')
+
+		@self.app.route('/')
+		def index():
+			return render_template('index.html')
+
+		@self.app.route('/downloads')
+		def downloads():
+			return render_template('downloads.html')
+
+		@self.app.route('/', methods=['POST'])
+		def my_form_post():
+		    text = request.form['text']
+		    processed_text = text.upper()
+		    return processed_text
+
+def setup(bot):
+    bot.add_cog(Flask(bot))
+
+
+
+
