94f485b8f6542e2606f45cd211d671b700efa1b4
Dunkledore <b7dunkerley@me.com>
2018-03-18 06:57:00 +0000

    tabs

--- cogs/flask.py
+++ cogs/flask.py
@@ -12,33 +12,33 @@ from threading import Thread
 import itertools, inspect
 
 def _command_signature(cmd):
-    # this is modified from discord.py source
-    # which I wrote myself lmao
+	# this is modified from discord.py source
+	# which I wrote myself lmao
 
-    result = [cmd.qualified_name]
-    if cmd.usage:
-        result.append(cmd.usage)
-        return ' '.join(result)
+	result = [cmd.qualified_name]
+	if cmd.usage:
+		result.append(cmd.usage)
+		return ' '.join(result)
 
-    params = cmd.clean_params
-    if not params:
-        return ' '.join(result)
+	params = cmd.clean_params
+	if not params:
+		return ' '.join(result)
 
-    for name, param in params.items():
-        if param.default is not param.empty:
-            # We don't want None or '' to trigger the [name=value] case and instead it should
-            # do [name] since [name=None] or [name=] are not exactly useful for the user.
-            should_print = param.default if isinstance(param.default, str) else param.default is not None
-            if should_print:
-                result.append(f'[{name}={param.default!r}]')
-            else:
-                result.append(f'[{name}]')
-        elif param.kind == param.VAR_POSITIONAL:
-            result.append(f'[{name}...]')
-        else:
-            result.append(f'<{name}>')
+	for name, param in params.items():
+		if param.default is not param.empty:
+			# We don't want None or '' to trigger the [name=value] case and instead it should
+			# do [name] since [name=None] or [name=] are not exactly useful for the user.
+			should_print = param.default if isinstance(param.default, str) else param.default is not None
+			if should_print:
+				result.append(f'[{name}={param.default!r}]')
+			else:
+				result.append(f'[{name}]')
+		elif param.kind == param.VAR_POSITIONAL:
+			result.append(f'[{name}...]')
+		else:
+			result.append(f'<{name}>')
 
-    return ' '.join(result)
+	return ' '.join(result)
 
 class Website:
 	"""The Welcome Related Commands"""
@@ -77,7 +77,7 @@ class Website:
 	def get_cogs(self):
 		
 		def key(c):
-    		return c.cog_name or '\u200bMisc'
+			return c.cog_name or '\u200bMisc'
 
 		entries = sorted(self.bot.commands, key=key)
 		display_commands = []
@@ -87,36 +87,36 @@ class Website:
 		# ...
 
 		for cog, commands in itertools.groupby(entries, key=key):
-		    non_hidden = [cmd for cmd in commands if not cmd.hidden]
-		    non_hidden = sorted(non_hidden, key=lambda x: x.name)
-		    if len(non_hidden) == 0:
-		        continue
+			non_hidden = [cmd for cmd in commands if not cmd.hidden]
+			non_hidden = sorted(non_hidden, key=lambda x: x.name)
+			if len(non_hidden) == 0:
+				continue
 
-		    description = ctx.bot.get_cog(cog)
-		    if description is not None:
-		        description = inspect.getdoc(description) or None
-		    detailed_commands = []
+			description = ctx.bot.get_cog(cog)
+			if description is not None:
+				description = inspect.getdoc(description) or None
+			detailed_commands = []
 
-		    for command in non_hidden:
-		        sig = (_command_signature(command))
-		        desc = command.short_doc or "No help given"
-		        detailed_commands.append({"signature" : sig, "description" : desc})
+			for command in non_hidden:
+				sig = (_command_signature(command))
+				desc = command.short_doc or "No help given"
+				detailed_commands.append({"signature" : sig, "description" : desc})
 
 
-		    display_commands.append({"name" : cog, "description" : description, "commands" : detailed_commands})
+			display_commands.append({"name" : cog, "description" : description, "commands" : detailed_commands})
 
-		    return display_commands
+			return display_commands
 
 
 
 
 def setup(bot):
-    bot.add_cog(Website(bot))
+	bot.add_cog(Website(bot))
 
 
 
 def key(c):
-    return c.cog_name or '\u200bMisc'
+	return c.cog_name or '\u200bMisc'
 
 entries = sorted(ctx.bot.commands, key=key)
 display_commands = []
@@ -126,23 +126,23 @@ display_commands = []
 # ...
 
 for cog, commands in itertools.groupby(entries, key=key):
-    non_hidden = [cmd for cmd in commands if not cmd.hidden]
-    non_hidden = sorted(non_hidden, key=lambda x: x.name)
-    if len(non_hidden) == 0:
-        continue
+	non_hidden = [cmd for cmd in commands if not cmd.hidden]
+	non_hidden = sorted(non_hidden, key=lambda x: x.name)
+	if len(non_hidden) == 0:
+		continue
 
-    description = ctx.bot.get_cog(cog)
-    if description is not None:
-        description = inspect.getdoc(description) or None
-    detailed_commands = []
+	description = ctx.bot.get_cog(cog)
+	if description is not None:
+		description = inspect.getdoc(description) or None
+	detailed_commands = []
 
-    for command in non_hidden:
-        sig = (_command_signature(command))
-        desc = command.short_doc or "No help given"
-        detailed_commands.append({"signature" : sig, "description" : desc})
+	for command in non_hidden:
+		sig = (_command_signature(command))
+		desc = command.short_doc or "No help given"
+		detailed_commands.append({"signature" : sig, "description" : desc})
 
 
-    display_commands.append({"name" : cog, "description" : description, "commands" : detailed_commands})
+	display_commands.append({"name" : cog, "description" : description, "commands" : detailed_commands})
 
 
 
