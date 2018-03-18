5ae30ce78e21ebac8f1b01e2e8647776f5e2510c
Dunkledore <b7dunkerley@me.com>
2018-03-18 06:26:53 +0000

    check

--- cogs/flask.py
+++ cogs/flask.py
@@ -28,6 +28,7 @@ class Website:
 		#self.app.add_url_rule('/', 'index,', self.index)
 		func = functools.partial(self.app.run, port=80)
 		await self.bot.loop.run_in_executor(None, func)
+		await ctx.send("running")
 
 
 
