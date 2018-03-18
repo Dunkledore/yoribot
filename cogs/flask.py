6814e3db4a9afc6545463fdcabf8be7f06b97e8d
Dunkledore <b7dunkerley@me.com>
2018-03-18 07:51:39 +0000

    badly

--- cogs/flask.py
+++ cogs/flask.py
@@ -22,22 +22,22 @@ TOKEN_URL = API_BASE_URL + '/oauth2/token'
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
 
 def token_updater(token):
-    session['oauth2_token'] = token
+	session['oauth2_token'] = token
 
 
 def make_session(token=None, state=None, scope=None):
-    return OAuth2Session(
-        client_id=OAUTH2_CLIENT_ID,
-        token=token,
-        state=state,
-        scope=scope,
-        redirect_uri=OAUTH2_REDIRECT_URI,
-        auto_refresh_kwargs={
-            'client_id': OAUTH2_CLIENT_ID,
-            'client_secret': OAUTH2_CLIENT_SECRET,
-        },
-        auto_refresh_url=TOKEN_URL,
-        token_updater=token_updater)
+	return OAuth2Session(
+		client_id=OAUTH2_CLIENT_ID,
+		token=token,
+		state=state,
+		scope=scope,
+		redirect_uri=OAUTH2_REDIRECT_URI,
+		auto_refresh_kwargs={
+			'client_id': OAUTH2_CLIENT_ID,
+			'client_secret': OAUTH2_CLIENT_SECRET,
+		},
+		auto_refresh_url=TOKEN_URL,
+		token_updater=token_updater)
 
 def _command_signature(cmd):
 	# this is modified from discord.py source
@@ -90,35 +90,35 @@ class Website:
 
 		@self.app.route('/me')
 		async def profile():
-		    if session.get('oauth2_token'):
-		        discord = make_session(token=session.get('oauth2_token'))
-		        session['guilds'] = discord.get(API_BASE_URL + '/users/@me/guilds').json()
-		        session['user'] = discord.get(API_BASE_URL + '/users/@me').json()
-		        session['user_connections'] = discord.get(API_BASE_URL + '/users/@me/connections').json()
-		        session['profile'] = self.fetch_profile()
+			if session.get('oauth2_token'):
+				discord = make_session(token=session.get('oauth2_token'))
+				session['guilds'] = discord.get(API_BASE_URL + '/users/@me/guilds').json()
+				session['user'] = discord.get(API_BASE_URL + '/users/@me').json()
+				session['user_connections'] = discord.get(API_BASE_URL + '/users/@me/connections').json()
+				session['profile'] = self.fetch_profile()
 			return await render_template('profile.html')
 
 		@self.app.route('/callback')
 		async def callback():
-		    if request.args.get('error'):
-		        return request.args['error']
-		    discord = make_session(state=session.get('oauth2_state'))
-		    token = discord.fetch_token(
-		        TOKEN_URL,
-		        client_secret=OAUTH2_CLIENT_SECRET,
-		        code=request.args.get('code'))
-		    session['oauth2_token'] = token
-		    return redirect('/me')
+			if request.args.get('error'):
+				return request.args['error']
+			discord = make_session(state=session.get('oauth2_state'))
+			token = discord.fetch_token(
+				TOKEN_URL,
+				client_secret=OAUTH2_CLIENT_SECRET,
+				code=request.args.get('code'))
+			session['oauth2_token'] = token
+			return redirect('/me')
 
 		@self.app.route('/login')
 		async def login():
-		    scope = request.args.get(
-		        'scope',
-		        'identify connections guilds')
-		    discord = make_session(scope=scope.split(' '))
-		    authorization_url, state = discord.authorization_url(AUTHORIZATION_BASE_URL)
-		    session['oauth2_state'] = state
-		    return redirect(authorization_url)
+			scope = request.args.get(
+				'scope',
+				'identify connections guilds')
+			discord = make_session(scope=scope.split(' '))
+			authorization_url, state = discord.authorization_url(AUTHORIZATION_BASE_URL)
+			session['oauth2_state'] = state
+			return redirect(authorization_url)
 
 		@self.app.route('/about')
 		async def about():
