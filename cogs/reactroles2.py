058802e01cedccaa2180c4aedd2f49c59cbf0542
Dunkledore <b7dunkerley@me.com>
2018-03-22 22:53:58 +0000

    name

--- cogs/reactroles2.py
+++ cogs/reactroles2.py
@@ -185,6 +185,8 @@ class ReactRoles:
 			else:
 				compare_emoji = emoji.name
 
+		await hook.send("name is {}".format(compare_emoji))
+
 			query = "SELECT * FROM reactroles WHERE message_id = $1"
 			results = await self.bot.pool.fetch(query, message_id)
 
