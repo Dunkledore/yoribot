f0c8244b0d2684d79c33fd53e7c28d494a470f08
Dunkledore <b7dunkerley@me.com>
2018-03-22 21:36:44 +0000

    eh

--- cogs/mod.py
+++ cogs/mod.py
@@ -211,7 +211,7 @@ class Mod:
         dataIO.save_json(self.location, self.json)
 
     @commands.command()
-    async def temp_status():
+    async def temp_status(self, ctx):
         await self.bot.change_presence(activity=discord.Game(name="yoribot.com"))
         await ctx.send("done")
 
