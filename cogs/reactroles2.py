561e53af9fc93291695f64b7ce38e2958bdbf683
Dunkledore <b7dunkerley@me.com>
2018-03-22 14:07:49 +0000

    4

--- cogs/reactroles2.py
+++ cogs/reactroles2.py
@@ -24,7 +24,7 @@ class ReactRoles:
 
 		emoji = reaction.emoji
 
-		query = "INSERT INTO reactroles (message_id, role_id, emoji_id, guild_id) VALUES $1, $2"
+		query = "INSERT INTO reactroles (message_id, role_id, emoji_id, guild_id) VALUES $1, $2, $3, $4"
 		await self.bot.pool.execute(query, message_id, role.id, str(emoji.id) or str(emoji) , ctx.guild.id)
 		await ctx.send("Emoji set to {} for {}".format(emoji, role.name))
 
