73478905ab0f330a39328aedbb91bfc8d1535215
Dunkledore <b7dunkerley@me.com>
2018-03-24 14:33:36 +0000

    more details

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -53,12 +53,15 @@ class Utilities:
                 if cog not in cogs:
                     cogs.append(cog)
 
-            #embed = discord.Embed(Title=discord.Embed.Empty, description="Here is a full list of catagories. Use `*help <category_name>` for more info on a specific category. For example. `*help Mod` for moderation specific commands ")
+            #embed = discord.Embed(Title=discord.Embed.Empty, description="Full list of catagories. Use `*help <category_name>` for more info on a specific category. For example. `*help Mod` for moderation specific commands ")
             #embed.add_field(name="Catagories", value=cog_string)
 
             #embed.set_author(name="Yori Bot Help", icon_url="http://yoribot.com/wp-content/uploads/2017/11/yoriicon.png")
             #embed.set_footer(text="For a list of all commands use *help all")
-            p = Pages(ctx, entries=cogs, per_page=10)
+            p = Pages(ctx, entries=cogs, per_page=15)
+            p.embed.description = "Full list of catagories. Use `*help <category_name>` for more info on a specific category. For example. `*help Mod` for moderation specific commands"
+            p.embed.set_author(name="Yori Bot Help", icon_url="http://yoribot.com/wp-content/uploads/2017/11/yoriicon.png")
+            p.embed.set_footer(text="For a list of all commands use *help all")
             await p.paginate()
             return
 
