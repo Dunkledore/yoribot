a3588b0b1655f4fea57883f5ac3cb1e7ab823a4f
Dunkledore <b7dunkerley@me.com>
2018-03-25 22:53:49 +0100

    removing these

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -51,13 +51,9 @@ class Utilities:
             import itertools
             for cog, cmds in itertools.groupby(entries, key=key):
                 if cog not in cogs:
-                    cogs.append(cog)
+                    if cog not in ["Admin", "YoriBot", "terminal"]:
+                        cogs.append(cog)
 
-            #embed = discord.Embed(Title=discord.Embed.Empty, description="Full list of catagories. Use `*help <category_name>` for more info on a specific category. For example. `*help Mod` for moderation specific commands ")
-            #embed.add_field(name="Catagories", value=cog_string)
-
-            #embed.set_author(name="Yori Bot Help", icon_url="http://yoribot.com/wp-content/uploads/2017/11/yoriicon.png")
-            #embed.set_footer(text="For a list of all commands use *help all")
             p = FirstHelpPaginator(ctx, entries=cogs, per_page=15)
             p.embed.set_author(name="Yori Bot Help", icon_url="http://yoribot.com/wp-content/uploads/2017/11/yoriicon.png")
             await p.paginate()
