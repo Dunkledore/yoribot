03da22cabbf5062429d08d3b2e65f589720ec78a
James <eadesjames@me.com>
2018-04-04 12:39:55 -0400

    dunk is scrub

--- cogs/warnings.py
+++ cogs/warnings.py
@@ -5,7 +5,7 @@ import datetime
 
 from .utils import checks
 
- #create table warnconfig (id SERIAL, guild_id BIGINT, muted_count INT,  muted_role BIGINT, banned_count INT, report_channel INT)
+ #create table warnconfig (id SERIAL, guild_id BIGINT unique, muted_count INT,  muted_role BIGINT, banned_count INT, report_channel INT)
  #create table warnings(id SERIAL, guild_id BIGINT, mod_id BIGINT, user_id BIGINT, reason TEXT, warning bool, date timestamp default current_timestamp)
 
 class Warnings:
