efcbbb62a8aef0b45df0b0cf014a21e393b5af7d
Dunkledore <b7dunkerley@me.com>
2018-04-16 13:30:19 +0200

    await

--- cogs/economy.py
+++ cogs/economy.py
@@ -68,7 +68,7 @@ class Economy():
 				currency = config["currency"]
 				await hook.send("got currency")
 				msg = await channel.send(embed=bankmanagerembed("{}{} are waiting to be claimed use click the {} to claim".format(drop_amount, currency, self.pick_emoji)))
-				msg.add_reaction(self.pick_emoji)
+				await msg.add_reaction(self.pick_emoji)
 
 				def check(reaction, user):
 					return (reaction.emoji == self.pick_emoji and reaction.message.id == msg.id)
