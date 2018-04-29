4f070c729351c52866f7bc384f2c534c6eb2e89d
James <eadesjames@me.com>
2018-04-29 17:27:41 -0400

    remove delete_message(x)

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -12,7 +12,7 @@ class Dictionary:
 	@commands.command(name="define", pass_context=True)
 	async def define(self, ctx, *, word: str):
 		"""Displays definitions of a given word"""
-		x = await ctx.send("Searching...")
+		await ctx.send("Searching...")
 		search_term = word.split(" ", 1)[0]
 		result = self.dictionary.meaning(search_term)
 		str_buffer = ""
@@ -36,33 +36,30 @@ class Dictionary:
 					else:
 						str_buffer += str(counter) + ". " + val + "\n"
 						counter += 1
-		await self.bot.delete_message(x)
 		await ctx.send(str_buffer)
 
 	@commands.command(name="antonym", pass_context=True)
 	async def antonym(self, ctx, *, word: str):
 		"""Displays antonyms for a given word"""
-		x = await ctx.send("Searching...")
+		await ctx.send("Searching...")
 		search_term = word.split(" ", 1)[0]
 		result = self.dictionary.antonym(search_term)
 		if result is None:
 			await self.bot.delete_message(x)
 			await ctx.send("This word is not in the dictionary.")
 			return
-		await self.bot.delete_message(x)
 		await ctx.send("Antonyms for **" + search_term + "**: *" + "*, *".join(result) + "*")
 
 	@commands.command(name="synonym", pass_context=True)
 	async def synonym(self, ctx, *, word: str):
 		"""Displays synonyms for a given word"""
-		x = await ctx.send("Searching...")
+		await ctx.send("Searching...")
 		search_term = word.split(" ", 1)[0]
 		result = self.dictionary.synonym(search_term)
 		if result is None:
 			await self.bot.delete_message(x)
 			await ctx.send("This word is not in the dictionary.")
 			return
-		await self.bot.delete_message(x)
 		await ctx.send("Synonyms for **" + search_term + "**: *" + "*, *".join(result) + "*")
 
 
