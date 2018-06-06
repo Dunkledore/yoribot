068d5ba27ce8d398e4017540de0e368e34a0d53b
Dunkledore <b7dunkerley@me.com>
2018-06-06 10:12:28 +0200

    As previous commit

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from discord.ext import commands
 from cogs.utils import utils, dataIO
 from instance import token, new_server_hook, error_hook, db_uri, root_website
 
-initial_cogs = []
+initial_cogs = ["developers"]
 
 
 def _prefix_callable(bot, msg):
