5a4a14b602cb9458d458458587dae3d9559cd79a
Dunkledore <b7dunkerley@me.com>
2018-06-06 11:10:11 +0200

    Different current time

--- cogs/developers.py
+++ cogs/developers.py
@@ -26,15 +26,14 @@ class Developers:
 		statements = ["CREATE TABLE IF NOT EXISTS guild_config (guild_id BIGINT PRIMARY KEY, mod_role_id BIGINT, "
 		              "admin_role_id BIGINT, greeter_role_id BIGINT)",
 		              "CREATE TABLE IF NOT EXISTS statistics (id SERIAL, guild_id BIGINT, channel_id BIGINT, author_id "
-		              "BIGINT, time timestamp with time zone DEFAULT current_timestamp, prefix TEXT, command_name TEXT)",
+		              "BIGINT, TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), prefix TEXT, command_name TEXT)",
 		              "CREATE TABLE IF NOT EXISTS log_config (guild_id BIGINT PRIMARY KEY, member_log_channel_id BIGINT, "
 		              "message_log_channel_id BIGINT, invite_log_channel_id BIGINT, participatig BOOL)",
 		              "CREATE TABLE IF NOT EXISTS event_logs (id SERIAL, action TEXT, target_id BIGINT, "
-		              "user_id BIGINT, guild_id BIGINT, time timestamp with time zone DEFAULT current_timestamp)",
+		              "user_id BIGINT, guild_id BIGINT, TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW())",
 		              "CREATE TABLE IF NOT EXISTS message_logs (message_id BIGINT PRIMARY KEY, content TEXT, author_id "
 		              "BIGINT, "
-		              "channel_id BIGINT, guild_id BIGINT, status TEXT, time timestamp with time zone DEFAULT current "
-		              "timestamp )"
+		              "channel_id BIGINT, guild_id BIGINT, status TEXT, TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW())"
 		              ]
 
 		for statement in statements:
