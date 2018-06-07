bd76339715026aaae9a2f99f5d592e799fd91ae0
Dunkledore <b7dunkerley@me.com>
2018-06-07 14:04:25 +0200

    target

--- cogs/website.py
+++ cogs/website.py
@@ -8,7 +8,7 @@ def add_views(website : Quart, bot):
 
 	@website.route('/logs/<int:guild_id>/<int:user_id>', methods=['GET'])
 	async def logs_for_user(guild_id, user_id):
-		query = "SELECT * FROM event_logs WHERE (guild_id = $1) and (user_id = $2)"
+		query = "SELECT * FROM event_logs WHERE (guild_id = $1) and (target_id = $2)"
 		logs = await bot.pool.fetch(query, guild_id, user_id)
 		if not logs:
 			return f"No logs for user {user_id}"
