6882de744a833582ae60e7d8392028e95df5722c
Dunkledore <b7dunkerley@me.com>
2018-06-07 14:10:37 +0200

    see logs

--- cogs/website.py
+++ cogs/website.py
@@ -12,6 +12,7 @@ def add_views(website : Quart, bot):
 		logs = await bot.pool.fetch(query, guild_id, user_id)
 		if not logs:
 			return f"No logs for user {user_id}"
+		print(logs)
 		return pformat(logs)
 
 
