c18fc40e5d1e3f9628296d2ca7ce985dcc8f080a
Dunkledore <b7dunkerley@me.com>
2018-09-26 12:27:12 +0200

    adding weather table

--- cogs/developers.py
+++ cogs/developers.py
@@ -116,7 +116,8 @@ class Developers():
 		              "CREATE TABLE IF NOT EXISTS react_roles (id SERIAL, message_id BIGINT, role_id BIGINT, emoji_id TEXT, guild_id BIGINT)",
 		              "CREATE TABLE IF NOT EXISTS rift (rift_name TEXT PRIMARY KEY, owner BIGINT, channels BIGINT[], blacklist BIGINT[])",
 		              "CREATE TABLE IF NOT EXISTS strike_config (guild_id BIGINT PRIMARY KEY, caps_ban INT, caps_mute INT, mention_ban INT, mention_mute INT, image_ban INT, image_mute INT, censor_ban INT, censor_mute INT)",
-		              "CREATE TABLE IF NOT EXISTS strikes (user_id BIGINT, guild_id BIGINT, caps_strikes INT DEFAULT 0, mention_strikes INT DEFAULT 0, censor_strikes INT DEFAULT 0, image_strikes INT DEFAULT 0, PRIMARY KEY (guild_id, user_id))"
+		              "CREATE TABLE IF NOT EXISTS strikes (user_id BIGINT, guild_id BIGINT, caps_strikes INT DEFAULT 0, mention_strikes INT DEFAULT 0, censor_strikes INT DEFAULT 0, image_strikes INT DEFAULT 0, PRIMARY KEY (guild_id, user_id))",
+		              "CREATE TABLE IF NOT EXISTS weather (key TEXT)"
 		              ]
 
 		for statement in statements:
