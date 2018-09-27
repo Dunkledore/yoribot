b7c8ef2e9d21f3f3d045aeb3cc614de0a0e694b8
Dunkledore <b7dunkerley@me.com>
2018-09-27 15:50:19 +0200

    Adding Urban

--- cogs/searches.py
+++ cogs/searches.py
@@ -24,6 +24,7 @@ class Searches:
 				author = data[0]["author"]
 				definition = data[0]["definition"]
 				embed = Embed(title=f"{word.title()} by {author.title()}", description=definition, url=url)
+				embed.set_footer(text="Powered By UrbanDictionary", icon_url="https://d2gatte9o95jao.cloudfront.net/assets/logo-1b439b7fa6572b659fbef161d8946372f472ef8e7169db1e47d21c91b410b918.svg")
 				await ctx.send(embed=embed)
 
 
