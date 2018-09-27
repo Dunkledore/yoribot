5d50e92857dc8affd845a323a6da5988d3821a9b
Dunkledore <b7dunkerley@me.com>
2018-09-27 19:18:16 +0200

    Adding dad joke

--- cogs/searches.py
+++ cogs/searches.py
@@ -1,16 +1,18 @@
 from discord.ext import commands
 import aiohttp
 from discord import Embed
+import asyncio
 
 
 class Searches:
-	"""Search different providers for information. (Google, Ubran Dictionary"""
+	"""Search different providers for information. (Google, Urban Dictionary...)"""
 
 	def __init__(self, bot):
 		self.bot = bot
 
 	@commands.command(aliases=["ubrandictionary", "ud"])
 	async def urban(self, ctx, *, word):
+		"""Search urban dictionary"""
 		async with aiohttp.ClientSession() as cs:
 			payload = {'term': word}
 			url = "http://api.urbandictionary.com/v0/define"
@@ -24,9 +26,26 @@ class Searches:
 				author = data[0]["author"]
 				definition = data[0]["definition"]
 				embed = Embed(title=f"{word.title()} by {author.title()}", description=definition, url=url)
-				embed.set_footer(text="Powered By UrbanDictionary", icon_url="https://d2gatte9o95jao.cloudfront.net/assets/logo-1b439b7fa6572b659fbef161d8946372f472ef8e7169db1e47d21c91b410b918.svg")
+				embed.set_footer(text="Powered By UrbanDictionary", icon_url="https://img.lgbtdis.co/images/2018/09/27/58a9c165e53c370ac4be52a7a98c35c3.md.png")
 				await ctx.send(embed=embed)
 
+	@commands.command()
+	async def dadjoke(self, ctx):
+		"""See a dadjoke"""
+		def make_embed(text):
+			embed = Embed(title="DadJoke", description=text)
+			embed.set_footer(text="Dad Joke provide by https://icanhazdadjoke.com")
+			return embed
+		async with aiohttp.ClientSession() as cs:
+			url = 'https://icanhazdadjoke.com/'
+			headers = {'Accept': 'text/plain'}
+			async with cs.get(url, headers=headers):
+				joke = await r.text()
+				head, sep, tail = joke.partition('?' or '.')
+				await ctx.send(embed=make_embed(head))
+				if tail:
+					asyncio.sleep(5)
+					await ctx.send(embed=make_embed(tail))
 
 def setup(bot):
 	bot.add_cog(Searches(bot))
