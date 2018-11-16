e6a7b2a4fe4d77039271c771600ac1c8ae24d96a
Dunkledore <b7dunkerley@me.com>
2018-11-16 08:12:45 +0200

    adding about

--- working_cogs/statistics.py
+++ working_cogs/statistics.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
-from discord import Embed
+from discord import Embed, Colour
+import psutil
 
 
 class Statistics:
@@ -7,6 +8,7 @@ class Statistics:
 	def __init__(self, bot):
 		self.bot = bot
 		self.category = "Misc"
+		self.process = psutil.Process()
 
 	async def on_command(self, ctx):
 		query = "INSERT INTO statistics (guild_id, channel_id, author_id, prefix, command_name) VALUES ($1, $2, $3, $4, $5)"
@@ -54,6 +56,45 @@ class Statistics:
 		                value='\n'.join([f'{channel} - {uses}' for channel, uses in channel_stats.items()]))
 
 		await ctx.send(embed=embed)
+
+
+@commands.command()
+async def about(self, ctx):
+	"""Tells you information about the bot itself."""
+
+	embed = Embed()
+	embed.title = 'About Yori Bot'
+	embed.url = 'http://yoribot.com'
+	embed.colour = Colour.blurple()
+
+	embed.set_author(name=ctx.bot.user.name, icon_url=ctx.bot.user.avatar_url)
+
+	# statistics
+	total_members = sum(1 for _ in self.bot.get_all_members())
+	total_online = len({m.id for m in self.bot.get_all_members() if m.status is discord.Status.online})
+	total_unique = len(self.bot.users)
+
+	voice_channels = []
+	text_channels = []
+	for guild in self.bot.guilds:
+		voice_channels.extend(guild.voice_channels)
+		text_channels.extend(guild.text_channels)
+
+	text = len(text_channels)
+	voice = len(voice_channels)
+
+	embed.add_field(name='Members', value=f'{total_members} total\n{total_unique} unique\n{total_online} unique online')
+	embed.add_field(name='Channels', value=f'{text + voice} total\n{text} text\n{voice} voice')
+
+	memory_usage = self.process.memory_full_info().uss/1024**2
+	cpu_usage = self.process.cpu_percent()/psutil.cpu_count()
+	embed.add_field(name='Process', value=f'{memory_usage:.2f} MiB\n{cpu_usage:.2f}% CPU')
+
+	embed.add_field(name='Guilds', value=len(self.bot.guilds))
+	embed.add_field(name='Commands Run', value=sum(self.bot.command_stats.values()))
+	embed.add_field(name='Website', value="[http://yoribot.com](http://yoribot.com)")
+	embed.set_footer(text='Made with discord.py', icon_url='http://i.imgur.com/5BFecvA.png')
+	await ctx.send(embed=embed)
 
 
 def setup(bot):
